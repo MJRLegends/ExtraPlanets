6accf990c318aaa87728c183d39e00c329bcb81c
MJRLegends <MJRLegends@users.noreply.github.com>
2015-11-16 23:39:05 +0000

    Language file fixes, Disabled the Multi Biome feature from Neptune and enabled the one biome system.

--- src/main/java/com/mjr/extraplanets/planets/Neptune/WorldProviderNeptune.java
+++ src/main/java/com/mjr/extraplanets/planets/Neptune/WorldProviderNeptune.java
@@ -10,8 +10,8 @@ import net.minecraft.world.biome.WorldChunkManager;
 import net.minecraft.world.chunk.IChunkProvider;
 
 import com.mjr.extraplanets.planets.PlanetsMain;
-import com.mjr.extraplanets.planets.Neptune.worldgen.ChunkProviderNeptuneNew;
-import com.mjr.extraplanets.planets.Neptune.worldgen.WorldChunkManagerNeptuneNew;
+import com.mjr.extraplanets.planets.Neptune.worldgen.ChunkProviderNeptune;
+import com.mjr.extraplanets.planets.Neptune.worldgen.WorldChunkManagerNeptune;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -54,12 +54,12 @@ IGalacticraftWorldProvider, ISolarLevel {
 
 	@Override
 	public Class<? extends IChunkProvider> getChunkProviderClass() {
-		return ChunkProviderNeptuneNew.class;
+		return ChunkProviderNeptune.class;
 	}
 
 	@Override
 	public Class<? extends WorldChunkManager> getWorldChunkManagerClass() {
-		return WorldChunkManagerNeptuneNew.class;
+		return WorldChunkManagerNeptune.class;
 	}
 
 	@Override
