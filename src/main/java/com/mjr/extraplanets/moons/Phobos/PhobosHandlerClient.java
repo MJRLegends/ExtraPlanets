a74e566fd19e05be668533665973b3851fb38fce
MJRLegends <MJRLegends@users.noreply.github.com>
2015-11-17 00:03:50 +0000

    *Added IO, Europa, Deimos, Phobos, Triton, Ganymede, Callisto Sky Providers and Client Handlers

---
+++ src/main/java/com/mjr/extraplanets/moons/Phobos/PhobosHandlerClient.java
@@ -0,0 +1,36 @@
+package com.mjr.extraplanets.moons.Phobos;
+
+import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
+import micdoodle8.mods.galacticraft.core.client.CloudRenderer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class PhobosHandlerClient {
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onClientTick(ClientTickEvent event)
+	{
+		Minecraft minecraft = Minecraft.getMinecraft();
+		WorldClient world = minecraft.theWorld;
+
+		if (world != null)
+		{
+			if (world.provider instanceof WorldProviderPhobos)
+			{
+				if (world.provider.getSkyRenderer() == null)
+				{
+					world.provider.setSkyRenderer(new SkyProviderPhobos((IGalacticraftWorldProvider) world.provider));
+				}
+
+				if (world.provider.getCloudRenderer() == null)
+				{
+					world.provider.setCloudRenderer(new CloudRenderer());
+				}
+			}
+		}
+	}
+}
