0efdc8bec7bad0b72621835af72f949737d0712e
MJRLegends <MJRLegends@users.noreply.github.com>
2016-12-08 11:43:25 +0000

    Fixed up the rest of the planet events

--- src/main/java/com/mjr/extraplanets/planets/Ceres/event/CeresEvents.java
+++ src/main/java/com/mjr/extraplanets/planets/Ceres/event/CeresEvents.java
@@ -2,15 +2,36 @@ package com.mjr.extraplanets.planets.Ceres.event;
 
 import micdoodle8.mods.galacticraft.api.event.oxygen.GCCoreOxygenSuffocationEvent;
 import micdoodle8.mods.galacticraft.core.event.EventWakePlayer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import com.mjr.extraplanets.Config;
 
 public class CeresEvents {
 	@SubscribeEvent
 	public void GCCoreOxygenSuffocationEvent(GCCoreOxygenSuffocationEvent.Pre event) {
-
+		if (event.entityLiving.worldObj.provider.getDimensionId() == Config.ceresID) {
+			if (event.entity instanceof EntityPlayer) {
+				event.setCanceled(false);
+			} else {
+				if (Config.mobSuffocation)
+					event.setCanceled(false);
+				else
+					event.setCanceled(true);
+			}
+		}
 	}
 
 	@SubscribeEvent
 	public void GCCoreEventWakePlayer(EventWakePlayer event) {
+		if (event.entityLiving.worldObj.provider.getDimensionId() == Config.ceresID) {
+			event.entityPlayer.heal(5.0F);
+
+			for (WorldServer worldServer : MinecraftServer.getServer().worldServers) {
+				worldServer.setWorldTime(0);
+			}
+		}
 	}
 }
