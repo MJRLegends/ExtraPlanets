ffb8eb74c5f1c94bf87b7072c55c7261b83e04d6
MJRLegends <MJRLegends@users.noreply.github.com>
2017-01-24 14:58:59 +0000

    Added Tooltips for Non JEI showing recipes, Tooltips will tell you what you need to craft them

--- src/main/java/com/mjr/extraplanets/items/ItemBasicItem.java
+++ src/main/java/com/mjr/extraplanets/items/ItemBasicItem.java
@@ -1,6 +1,14 @@
 package com.mjr.extraplanets.items;
 
+import java.util.List;
+
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import com.mjr.extraplanets.ExtraPlanets;
 
@@ -11,5 +19,20 @@ public class ItemBasicItem extends Item {
 		this.setHasSubtypes(true);
 		this.setUnlocalizedName(assetName);
 		this.setCreativeTab(ExtraPlanets.ItemsTab);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) {
+		if (player.worldObj.isRemote) {
+			if(itemStack.getItem() == ExtraPlanets_Items.tier1ArmorLayer)
+				list.add(EnumChatFormatting.GOLD + GCCoreUtil.translate("tier1ArmorLayer.desc"));
+			else if(itemStack.getItem() == ExtraPlanets_Items.tier2ArmorLayer)
+				list.add(EnumChatFormatting.GOLD + GCCoreUtil.translate("tier2ArmorLayer.desc"));
+			else if(itemStack.getItem() == ExtraPlanets_Items.tier3ArmorLayer)
+				list.add(EnumChatFormatting.GOLD + GCCoreUtil.translate("tier3ArmorLayer.desc"));
+			else if(itemStack.getItem() == ExtraPlanets_Items.tier4ArmorLayer)
+				list.add(EnumChatFormatting.GOLD + GCCoreUtil.translate("tier4ArmorLayer.desc"));
+		}
 	}
 }
--- src/main/java/com/mjr/extraplanets/items/ItemWafers.java
+++ src/main/java/com/mjr/extraplanets/items/ItemWafers.java
@@ -3,10 +3,13 @@ package com.mjr.extraplanets.items;
 import java.util.List;
 
 import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -45,5 +48,15 @@ public class ItemWafers extends Item {
 	@Override
 	public int getMetadata(int par1) {
 		return par1;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) {
+		if (player.worldObj.isRemote) {
+			for(int i = 0; i < names.length; i++)
+				if(itemStack.getMetadata() == i)
+					list.add(EnumChatFormatting.GOLD + GCCoreUtil.translate("wafers."+ this.names[i] + ".desc"));
+		}
 	}
 }
