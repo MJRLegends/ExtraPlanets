c9ebde8fe82e8b019ec2b065a0288d7eeb29adb4
MJRLegends <MJRLegends@users.noreply.github.com>
2017-03-06 13:33:09 +0000

    Fixes for Kepler Systems Config option

--- src/main/java/com/mjr/extraplanets/planets/ExtraPlanets_SpaceStations.java
+++ src/main/java/com/mjr/extraplanets/planets/ExtraPlanets_SpaceStations.java
@@ -146,7 +146,7 @@ public class ExtraPlanets_SpaceStations {
 			erisSpaceStation.setDimensionInfo(Config.erisSpaceStationID, Config.erisSpaceStationStaticID, WorldProviderErisOrbit.class);
 			erisSpaceStation.setBodyIcon(new ResourceLocation("galacticraftcore:textures/gui/celestialbodies/spaceStation.png"));
 		}
-		if (Config.kepler22bSpaceStation && Config.kepler22b) {
+		if (Config.kepler22bSpaceStation && Config.kepler22b && Config.keplerSolarSystems) {
 			kepler22bSpaceStation = new Satellite("spaceStation.kepler22b").setParentBody(ExtraPlanets_Planets.kepler22b);
 			kepler22bSpaceStation.setRelativeSize(0.2667F);
 			kepler22bSpaceStation.setRelativeDistanceFromCenter(new CelestialBody.ScalableDistance(5.5F, 5.5F));
@@ -218,7 +218,7 @@ public class ExtraPlanets_SpaceStations {
 			GalacticraftRegistry.registerProvider(Config.erisSpaceStationID, WorldProviderErisOrbit.class, false, 0);
 			GalacticraftRegistry.registerProvider(Config.erisSpaceStationStaticID, WorldProviderErisOrbit.class, true, 0);
 		}
-		if (Config.kepler22bSpaceStation && Config.kepler22b) {
+		if (Config.kepler22bSpaceStation && Config.kepler22b && Config.keplerSolarSystems) {
 			GalaxyRegistry.registerSatellite(kepler22bSpaceStation);
 			GalacticraftRegistry.registerTeleportType(WorldProviderKepler22bOrbit.class, new TeleportTypeKepler22bOrbit());
 			GalacticraftRegistry.registerProvider(Config.kepler22bSpaceStationID, WorldProviderKepler22bOrbit.class, false, 0);
