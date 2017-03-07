fef25fdd09a7b86ab26245ea0559932560d27b67
MJRLegends <MJRLegends@users.noreply.github.com>
2017-03-07 03:15:53 +0000

    Added Basic Hideout's to Jupiter

--- src/main/java/com/mjr/extraplanets/planets/Jupiter/worldgen/BiomeDecoratorJupiter.java
+++ src/main/java/com/mjr/extraplanets/planets/Jupiter/worldgen/BiomeDecoratorJupiter.java
@@ -9,6 +9,7 @@ import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 
 import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
 import com.mjr.extraplanets.blocks.fluid.ExtraPlanets_Fluids;
+import com.mjr.extraplanets.worldGen.features.WorldGenBasicHideout;
 import com.mjr.extraplanets.worldGen.features.WorldGenCustomLake;
 
 public class BiomeDecoratorJupiter extends BiomeDecoratorSpace {
@@ -71,6 +72,12 @@ public class BiomeDecoratorJupiter extends BiomeDecoratorSpace {
 				int y = this.currentWorld.getHeightValue(x, z);
 				new WorldGenCustomLake(ExtraPlanets_Fluids.magma).generate(this.currentWorld, this.rand, x, y, z, ExtraPlanets_Blocks.jupiterBlocks);
 			}
+		}
+		if (this.rand.nextInt(200) == 1) {
+			int x = this.chunkX + 8;
+			int z = this.chunkZ + 8;
+			int y = this.currentWorld.getHeightValue(x, z);
+			new WorldGenBasicHideout().generate(this.currentWorld, this.rand, x, y, z);
 		}
 		MinecraftForge.EVENT_BUS.post(new DecorateBiomeEvent.Post(this.currentWorld, this.rand, this.chunkX, this.chunkZ));
 
