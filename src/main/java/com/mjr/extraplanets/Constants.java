3da4a5af526fe1296697fa83697ed48dc3adb035
MJRLegends <MJRLegends@users.noreply.github.com>
2017-03-20 00:26:40 +0000

    Changed mod id to lowercase

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -1,11 +1,11 @@
 package com.mjr.extraplanets;
 
 public class Constants {
-	public static final String modID = "ExtraPlanets";
+	public static final String modID = "extraplanets";
 	public static final String modName = "ExtraPlanets";
-	public static final String modVersion = "0.6-Alpha-Build";
+	public static final String modVersion = "0.1-Alpha-Build";
 
-	public static final String ASSET_PREFIX = "extraplanets";
+	public static final String ASSET_PREFIX = modID;
 	public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
-	public static final String PREFIX = "extraplanets.";
+	public static final String PREFIX = modID + ".";
 }
