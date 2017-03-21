99e44f3724312e1526ed13dd713e9574e0d0f692
MJRLegends <MJRLegends@users.noreply.github.com>
2017-03-21 02:02:35 +0000

    Cleaned up the version number to a more organized version system

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -3,7 +3,7 @@ package com.mjr.extraplanets;
 public class Constants {
 	public static final String modID = "extraplanets";
 	public static final String modName = "ExtraPlanets";
-	public static final String modVersion = "0.1-Alpha-Build";
+	public static final String modVersion = "0.1.0-Alpha-Build";
 
 	public static final String ASSET_PREFIX = modID;
 	public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
