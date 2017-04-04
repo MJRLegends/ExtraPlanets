c517ae3fd2ac0ea8fc9d5da75ac4d0c9273d5f00
MJRLegends <MJRLegends@users.noreply.github.com>
2017-04-04 18:09:52 +0100

    Clean up: Remove unneeded code

--- src/main/java/com/mjr/extraplanets/recipes/ExtraPlanets_Recipes.java
+++ src/main/java/com/mjr/extraplanets/recipes/ExtraPlanets_Recipes.java
@@ -765,11 +765,7 @@ public class ExtraPlanets_Recipes {
 		GameRegistry.addShapelessRecipe(new ItemStack(ExtraPlanets_Items.cannedFood, 1, 2), new Object[] { new ItemStack(GCItems.canister, 1, 0), new ItemStack(Items.cooked_fish, 1, 1), new ItemStack(Items.cooked_fish, 1, 1) });
 		GameRegistry.addShapelessRecipe(new ItemStack(ExtraPlanets_Items.cannedFood, 1, 3), new Object[] { new ItemStack(GCItems.canister, 1, 0), Items.cooked_chicken, Items.cooked_chicken });
 		GameRegistry.addShapelessRecipe(new ItemStack(ExtraPlanets_Items.cannedFood, 1, 4), new Object[] { new ItemStack(GCItems.canister, 1, 0), Items.cooked_beef, Items.cooked_beef });
-
-		// Others
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.dye, 6, 15), new Object[] { new ItemStack(ExtraPlanets_Items.bodyParts, 1, 0) });
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.dye, 6, 15), new Object[] { new ItemStack(ExtraPlanets_Items.bodyParts, 1, 1) });
-
+		
 		// Advanced & Ultimate Battery
 		if (Config.batteries) {
 			GameRegistry.addRecipe(new ItemStack(ExtraPlanets_Items.advancedBattery, 1, 0),
