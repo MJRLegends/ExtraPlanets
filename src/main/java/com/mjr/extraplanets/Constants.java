763304b4bd7e2b16c3e15bb0953ab62e16f9718b
MJRLegends <MJRLegends@users.noreply.github.com>
2017-05-07 15:13:43 +0100

    Released 0.8.6-Beta-Build

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -3,7 +3,7 @@ package com.mjr.extraplanets;
 public class Constants {
 	public static final String modID = "ExtraPlanets";
 	public static final String modName = "ExtraPlanets";
-	public static final String modVersion = "0.8.5-Beta-Build";
+	public static final String modVersion = "0.8.6-Beta-Build";
 
 	public static final String ASSET_PREFIX = "extraplanets";
 	public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
