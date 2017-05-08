5fdfd95a60c75b7ea8ec6661c575f9a0b34039d3
MJRLegends <MJRLegends@users.noreply.github.com>
2017-05-08 14:12:56 +0100

    Updates/Tweaks for Venus Rover

--- src/main/java/com/mjr/extraplanets/client/render/entities/vehicles/RenderVenusRover.java
+++ src/main/java/com/mjr/extraplanets/client/render/entities/vehicles/RenderVenusRover.java
@@ -45,7 +45,7 @@ public class RenderVenusRover extends Render {
 		final float var24 = entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * par9;
 		GL11.glTranslatef((float) par2, (float) par4 - 2.5F, (float) par6);
 		GL11.glScalef(1.25F, 1.25F, 1.25F);
-		GL11.glRotatef(0.0F - par8, 0.0F, 1.0F, 0.0F);
+		GL11.glRotatef(180.0F - par8, 0.0F, 1.0F, 0.0F);
 		GL11.glRotatef(-var24, 0.0F, 0.0F, 1.0F);
 		GL11.glScalef(0.51F, 0.51F, 0.51F);
 		this.bindTexture(RenderVenusRover.buggyTextureWheel);
