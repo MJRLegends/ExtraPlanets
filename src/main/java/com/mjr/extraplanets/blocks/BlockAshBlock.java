0bbcad77d447d700da65beb9508df92362d52cfd
MJRLegends <MJRLegends@users.noreply.github.com>
2017-06-09 00:40:41 +0100

    Fixed issue #107

--- src/main/java/com/mjr/extraplanets/blocks/BlockAshBlock.java
+++ src/main/java/com/mjr/extraplanets/blocks/BlockAshBlock.java
@@ -1,13 +1,8 @@
 package com.mjr.extraplanets.blocks;
 
-import java.util.Random;
-
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 
 import com.mjr.extraplanets.Constants;
-import com.mjr.extraplanets.items.ExtraPlanets_Items;
 
 public class BlockAshBlock extends BlockFalling {
 	public BlockAshBlock(String name) {
@@ -16,10 +11,5 @@ public class BlockAshBlock extends BlockFalling {
 		setStepSound(Block.soundTypeGravel);
 		setBlockName(name);
 		setBlockTextureName(Constants.TEXTURE_PREFIX + name);
-	}
-
-	@Override
-	public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_) {
-		return new ItemStack(ExtraPlanets_Items.tier4Items, 1, 10).getItem();
 	}
 }
