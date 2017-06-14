cddee242f019fdf71cf937a355e7275ec88225eb
MJRLegends <MJRLegends@users.noreply.github.com>
2017-06-14 12:46:26 +0100

    Fixes for issue #108, Added some new Config options

--- src/main/java/com/mjr/extraplanets/world/WorldGenerationOverworld.java
+++ src/main/java/com/mjr/extraplanets/world/WorldGenerationOverworld.java
@@ -9,6 +9,7 @@ import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
+import com.mjr.extraplanets.Config;
 import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
 
 public class WorldGenerationOverworld implements IWorldGenerator {
@@ -23,12 +24,12 @@ public class WorldGenerationOverworld implements IWorldGenerator {
 	}
 
 	private void generateSurface(World world, Random rand, int chunkX, int chunkZ) {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < Config.ORE_LEAD_GENERATION_AMOUNT; i++) {
 			int randPosX = chunkX + rand.nextInt(16);
 			int randPosY = rand.nextInt(64);
 			int randPosZ = chunkZ + rand.nextInt(16);
 
-			(new WorldGenMinable(ExtraPlanets_Blocks.ORE_LEAD.getDefaultState(), 10)).generate(world, rand, new BlockPos(randPosX, randPosY, randPosZ));
+			(new WorldGenMinable(ExtraPlanets_Blocks.ORE_LEAD.getDefaultState(), Config.ORE_LEAD_GENERATION_AMOUNT)).generate(world, rand, new BlockPos(randPosX, randPosY, randPosZ));
 		}
 	}
 }
