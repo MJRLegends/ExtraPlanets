cb07f98372cba80c80df3700d2f1afd7daace793
MJRLegends <MJRLegends@users.noreply.github.com>
2017-06-14 21:17:54 +0100

    Even Even more fixes for Issue #109

--- src/main/java/com/mjr/extraplanets/inventory/vehicles/InventoryMarsRover.java
+++ src/main/java/com/mjr/extraplanets/inventory/vehicles/InventoryMarsRover.java
@@ -1,5 +1,6 @@
 package com.mjr.extraplanets.inventory.vehicles;
 
+import micdoodle8.mods.galacticraft.core.inventory.IInventoryDefaults;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
@@ -8,7 +9,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 
-public class InventoryMarsRover implements IInventory {
+public class InventoryMarsRover implements IInventoryDefaults {
 	private final NonNullList<ItemStack> stackList;
 	private final int inventoryWidth;
 	private final Container eventHandler;
@@ -21,28 +22,35 @@ public class InventoryMarsRover implements IInventory {
 	}
 
 	@Override
-	public int getSizeInventory() {
+    public int getSizeInventory()
+    {
         return this.stackList.size();
-	}
+    }
 
-	@Override
-	public ItemStack getStackInSlot(int par1) {
+    @Override
+    public ItemStack getStackInSlot(int par1)
+    {
         return par1 >= this.getSizeInventory() ? ItemStack.EMPTY : this.stackList.get(par1);
-	}
+    }
 
-	public ItemStack getStackInRowAndColumn(int par1, int par2) {
-		if (par1 >= 0 && par1 < this.inventoryWidth) {
-			final int var3 = par1 + par2 * this.inventoryWidth;
-			return this.getStackInSlot(var3);
-		} else {
-			return null;
-		}
-	}
+    public ItemStack getStackInRowAndColumn(int par1, int par2)
+    {
+        if (par1 >= 0 && par1 < this.inventoryWidth)
+        {
+            final int var3 = par1 + par2 * this.inventoryWidth;
+            return this.getStackInSlot(var3);
+        }
+        else
+        {
+            return ItemStack.EMPTY;
+        }
+    }
 
-	@Override
-	public String getName() {
-		return "container.crafting";
-	}
+    @Override
+    public String getName()
+    {
+        return "container.crafting";
+    }
 
     @Override
     public ItemStack removeStackFromSlot(int index)
@@ -83,62 +91,44 @@ public class InventoryMarsRover implements IInventory {
         this.eventHandler.onCraftMatrixChanged(this);
     }
 
-	@Override
-	public int getInventoryStackLimit() {
-		return 64;
-	}
+    @Override
+    public int getInventoryStackLimit()
+    {
+        return 64;
+    }
 
-	@Override
-	public void markDirty() {
-	}
+    @Override
+    public void markDirty()
+    {
+    }
 
-	@Override
-	public boolean isUsableByPlayer(EntityPlayer par1EntityPlayer) {
-		return true;
-	}
+    @Override
+    public boolean isUsableByPlayer(EntityPlayer par1EntityPlayer)
+    {
+        return true;
+    }
 
-	@Override
-	public void openInventory(EntityPlayer player) {
-	}
+    @Override
+    public void openInventory(EntityPlayer player)
+    {
+    }
 
-	@Override
-	public void closeInventory(EntityPlayer player) {
-	}
+    @Override
+    public void closeInventory(EntityPlayer player)
+    {
+    }
 
-	@Override
-	public boolean hasCustomName() {
-		return false;
-	}
+    @Override
+    public boolean hasCustomName()
+    {
+        return false;
+    }
 
-	@Override
-	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
-		return false;
-	}
-
-	@Override
-	public ITextComponent getDisplayName() {
-		return null;
-	}
-
-	@Override
-	public int getField(int id) {
-		return 0;
-	}
-
-	@Override
-	public void setField(int id, int value) {
-
-	}
-
-	@Override
-	public int getFieldCount() {
-		return 0;
-	}
-
-	@Override
-	public void clear() {
-
-	}
+    @Override
+    public boolean isItemValidForSlot(int i, ItemStack itemstack)
+    {
+        return false;
+    }
 
     @Override
     public boolean isEmpty()
