2886f62aca2c9433a21fcd051278a100600de05a
MJRLegends <MJRLegends@users.noreply.github.com>
2017-06-19 16:20:00 +0100

    Jupiter Biome fixes/Tweaks

--- src/main/java/com/mjr/extraplanets/planets/Jupiter/worldgen/biomes/BiomeGenJupiter.java
+++ src/main/java/com/mjr/extraplanets/planets/Jupiter/worldgen/biomes/BiomeGenJupiter.java
@@ -2,6 +2,7 @@ package com.mjr.extraplanets.planets.Jupiter.worldgen.biomes;
 
 import net.minecraftforge.common.BiomeDictionary;
 
+import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
 import com.mjr.extraplanets.planets.Jupiter.worldgen.JupiterBiomes;
 
 public class BiomeGenJupiter extends JupiterBiomes {
@@ -9,5 +10,7 @@ public class BiomeGenJupiter extends JupiterBiomes {
 	public BiomeGenJupiter(BiomeProperties properties) {
 		super(properties);
         BiomeDictionary.registerBiomeType(this, BiomeDictionary.Type.HOT, BiomeDictionary.Type.DRY, BiomeDictionary.Type.DEAD, BiomeDictionary.Type.SANDY);
+        this.topBlock = ExtraPlanets_Blocks.JUPITER_BLOCKS.getDefaultState();
+        this.fillerBlock = ExtraPlanets_Blocks.JUPITER_BLOCKS.getStateFromMeta(1);
 	}
 }
--- src/main/java/com/mjr/extraplanets/planets/Jupiter/worldgen/biomes/BiomeGenJupiterMagmaSea.java
+++ src/main/java/com/mjr/extraplanets/planets/Jupiter/worldgen/biomes/BiomeGenJupiterMagmaSea.java
@@ -2,6 +2,7 @@ package com.mjr.extraplanets.planets.Jupiter.worldgen.biomes;
 
 import net.minecraftforge.common.BiomeDictionary;
 
+import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
 import com.mjr.extraplanets.planets.Jupiter.worldgen.JupiterBiomes;
 
 public class BiomeGenJupiterMagmaSea extends JupiterBiomes {
@@ -9,5 +10,7 @@ public class BiomeGenJupiterMagmaSea extends JupiterBiomes {
 	public BiomeGenJupiterMagmaSea(BiomeProperties properties) {
 		super(properties);
         BiomeDictionary.registerBiomeType(this, BiomeDictionary.Type.HOT, BiomeDictionary.Type.OCEAN);
+        this.topBlock = ExtraPlanets_Blocks.JUPITER_BLOCKS.getDefaultState();
+        this.fillerBlock = ExtraPlanets_Blocks.JUPITER_BLOCKS.getStateFromMeta(1);
 	}
 }
