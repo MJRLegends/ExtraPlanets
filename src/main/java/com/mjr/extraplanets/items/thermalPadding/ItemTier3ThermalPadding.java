d046879972e1b982d4501777f557551ebb8a7d06
MJRLegends <MJRLegends@users.noreply.github.com>
2017-07-09 22:59:31 +0100

    Added Tooltips to Thermal Padding, Updated/Added Tooltips for Launch/Powered Custom Pads, Updated lang files

--- src/main/java/com/mjr/extraplanets/items/thermalPadding/ItemTier3ThermalPadding.java
+++ src/main/java/com/mjr/extraplanets/items/thermalPadding/ItemTier3ThermalPadding.java
@@ -1,9 +1,13 @@
 package com.mjr.extraplanets.items.thermalPadding;
 
+import java.util.List;
+
 import micdoodle8.mods.galacticraft.api.item.IItemThermal;
 import micdoodle8.mods.galacticraft.core.entities.player.GCCapabilities;
 import micdoodle8.mods.galacticraft.core.entities.player.GCPlayerStats;
 import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
+import micdoodle8.mods.galacticraft.core.util.EnumColor;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -20,120 +24,104 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import com.mjr.extraplanets.ExtraPlanets;
 
-public class ItemTier3ThermalPadding  extends Item implements IItemThermal
-{
-    public static String[] names = { "tier3_thermal_helm", "tier3_thermal_chestplate", "tier3_thermal_leggings", "tier3_thermal_boots", "tier3_thermal_helm0", "tier3_thermal_chestplate0", "tier3_thermal_leggings0", "tier3_thermal_boots0" };
+public class ItemTier3ThermalPadding extends Item implements IItemThermal {
+	public static String[] names = { "tier3_thermal_helm", "tier3_thermal_chestplate", "tier3_thermal_leggings", "tier3_thermal_boots", "tier3_thermal_helm0", "tier3_thermal_chestplate0", "tier3_thermal_leggings0", "tier3_thermal_boots0" };
 
-    public ItemTier3ThermalPadding(String assetName)
-    {
-        super();
-        this.setMaxDamage(0);
-        this.setHasSubtypes(true);
-        this.setMaxStackSize(1);
-        this.setUnlocalizedName(assetName);
-    }
+	public ItemTier3ThermalPadding(String assetName) {
+		super();
+		this.setMaxDamage(0);
+		this.setHasSubtypes(true);
+		this.setMaxStackSize(1);
+		this.setUnlocalizedName(assetName);
+	}
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public EnumRarity getRarity(ItemStack par1ItemStack)
-    {
-        return ClientProxyCore.galacticraftItem;
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public EnumRarity getRarity(ItemStack par1ItemStack) {
+		return ClientProxyCore.galacticraftItem;
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public CreativeTabs getCreativeTab()
-    {
-        return ExtraPlanets.ArmorTab;
-    }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public CreativeTabs getCreativeTab() {
+		return ExtraPlanets.ArmorTab;
+	}
 
-    @Override
-    public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, NonNullList<ItemStack> par3List)
-    {
-        for (int i = 0; i < ItemTier3ThermalPadding.names.length / 2; i++)
-        {
-            par3List.add(new ItemStack(par1, 1, i));
-        }
-    }
+	@Override
+	public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, NonNullList<ItemStack> par3List) {
+		for (int i = 0; i < ItemTier3ThermalPadding.names.length / 2; i++) {
+			par3List.add(new ItemStack(par1, 1, i));
+		}
+	}
 
-    @Override
-    public String getUnlocalizedName(ItemStack par1ItemStack)
-    {
-        if (names.length > par1ItemStack.getItemDamage())
-        {
-            return "item." + ItemTier3ThermalPadding.names[par1ItemStack.getItemDamage()];
-        }
+	@Override
+	public String getUnlocalizedName(ItemStack par1ItemStack) {
+		if (names.length > par1ItemStack.getItemDamage()) {
+			return "item." + ItemTier3ThermalPadding.names[par1ItemStack.getItemDamage()];
+		}
 
-        return "unnamed";
-    }
+		return "unnamed";
+	}
 
-    @Override
-    public int getMetadata(int par1)
-    {
-        return par1;
-    }
+	@Override
+	public int getMetadata(int par1) {
+		return par1;
+	}
 
-    @Override
-    public int getThermalStrength()
-    {
-        return 50;
-    }
+	@Override
+	public int getThermalStrength() {
+		return 50;
+	}
 
-    @Override
-    public boolean isValidForSlot(ItemStack stack, int armorSlot)
-    {
-        return stack.getItemDamage() == armorSlot;
-    }
-    
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand)
-    {
-        ItemStack itemStackIn = playerIn.getHeldItem(hand);
-        if (playerIn instanceof EntityPlayerMP)
-        {
-            GCPlayerStats stats = playerIn.getCapability(GCCapabilities.GC_STATS_CAPABILITY, null);
-            ItemStack gear = stats.getExtendedInventory().getStackInSlot(6);
-            ItemStack gear1 = stats.getExtendedInventory().getStackInSlot(7);
-            ItemStack gear2 = stats.getExtendedInventory().getStackInSlot(8);
-            ItemStack gear3 = stats.getExtendedInventory().getStackInSlot(9);
+	@Override
+	public boolean isValidForSlot(ItemStack stack, int armorSlot) {
+		return stack.getItemDamage() == armorSlot;
+	}
 
-            if (itemStackIn.getItemDamage() == 0)
-            {
-                if (gear.isEmpty())
-                {
-                    stats.getExtendedInventory().setInventorySlotContents(6, itemStackIn.copy());
-                    itemStackIn.setCount(0);
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		ItemStack itemStackIn = playerIn.getHeldItem(hand);
+		if (playerIn instanceof EntityPlayerMP) {
+			GCPlayerStats stats = playerIn.getCapability(GCCapabilities.GC_STATS_CAPABILITY, null);
+			ItemStack gear = stats.getExtendedInventory().getStackInSlot(6);
+			ItemStack gear1 = stats.getExtendedInventory().getStackInSlot(7);
+			ItemStack gear2 = stats.getExtendedInventory().getStackInSlot(8);
+			ItemStack gear3 = stats.getExtendedInventory().getStackInSlot(9);
 
-                }
-            }
-            else if (itemStackIn.getItemDamage() == 1)
-            {
-                if (gear1.isEmpty())
-                {
-                    stats.getExtendedInventory().setInventorySlotContents(7, itemStackIn.copy());
-                    itemStackIn.setCount(0);
+			if (itemStackIn.getItemDamage() == 0) {
+				if (gear.isEmpty()) {
+					stats.getExtendedInventory().setInventorySlotContents(6, itemStackIn.copy());
+					itemStackIn.setCount(0);
 
-                }
-            }
-            else if (itemStackIn.getItemDamage() == 2)
-            {
-                if (gear2.isEmpty())
-                {
-                    stats.getExtendedInventory().setInventorySlotContents(8, itemStackIn.copy());
-                    itemStackIn.setCount(0);
+				}
+			} else if (itemStackIn.getItemDamage() == 1) {
+				if (gear1.isEmpty()) {
+					stats.getExtendedInventory().setInventorySlotContents(7, itemStackIn.copy());
+					itemStackIn.setCount(0);
 
-                }
-            }
-            else if (itemStackIn.getItemDamage() == 3)
-            {
-                if (gear3.isEmpty())
-                {
-                    stats.getExtendedInventory().setInventorySlotContents(9, itemStackIn.copy());
-                    itemStackIn.setCount(0);
+				}
+			} else if (itemStackIn.getItemDamage() == 2) {
+				if (gear2.isEmpty()) {
+					stats.getExtendedInventory().setInventorySlotContents(8, itemStackIn.copy());
+					itemStackIn.setCount(0);
 
-                }
-            }
-        }
-        return new ActionResult<>(EnumActionResult.SUCCESS, itemStackIn);
-    }
+				}
+			} else if (itemStackIn.getItemDamage() == 3) {
+				if (gear3.isEmpty()) {
+					stats.getExtendedInventory().setInventorySlotContents(9, itemStackIn.copy());
+					itemStackIn.setCount(0);
+
+				}
+			}
+		}
+		return new ActionResult<>(EnumActionResult.SUCCESS, itemStackIn);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) {
+		if (player.world.isRemote) {
+			list.add(EnumColor.AQUA + GCCoreUtil.translate("tier3.thermal.padding.information"));
+		}
+	}
 }
