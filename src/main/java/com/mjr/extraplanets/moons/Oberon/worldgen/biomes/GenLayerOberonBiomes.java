2e6d3868f47e221ba4646cf5f003621d55e6f027
MJRLegends <MJRLegends@users.noreply.github.com>
2017-08-03 01:47:40 +0100

    Tweaked Oberon Large Ice Mountain biomes spawn rate again, and few fixes for Valley biome

--- src/main/java/com/mjr/extraplanets/moons/Oberon/worldgen/biomes/GenLayerOberonBiomes.java
+++ src/main/java/com/mjr/extraplanets/moons/Oberon/worldgen/biomes/GenLayerOberonBiomes.java
@@ -7,8 +7,8 @@ import net.minecraft.world.gen.layer.GenLayer;
 import com.mjr.extraplanets.moons.Oberon.worldgen.OberonBiomes;
 
 public class GenLayerOberonBiomes extends GenLayer {
-	private static final BiomeGenBase[] biomes = new BiomeGenBase[] { OberonBiomes.oberon, OberonBiomes.oberonLargeIceMountain };
-	private static final BiomeGenBase[] biomesRare = new BiomeGenBase[] { OberonBiomes.oberon };
+	private static final BiomeGenBase[] biomes = new BiomeGenBase[] { OberonBiomes.oberon, OberonBiomes.oberonValleys };
+	private static final BiomeGenBase[] biomesRare = new BiomeGenBase[] { OberonBiomes.oberonLargeIceMountain };
 
 	public GenLayerOberonBiomes(long l, GenLayer parent) {
 		super(l);
@@ -26,7 +26,7 @@ public class GenLayerOberonBiomes extends GenLayer {
 		for (int k = 0; k < depth; ++k) {
 			for (int i = 0; i < width; ++i) {
 				initChunkSeed(x + i, z + k);
-				if (this.nextInt(5) == 0) {
+				if (this.nextInt(10) == 0) {
 					dest[i + k * width] = biomesRare[nextInt(biomesRare.length)].biomeID;
 				} else {
 					dest[i + k * width] = biomes[nextInt(biomes.length)].biomeID;
