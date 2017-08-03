0b20f41876b201bf3a775627e4e610aa50a208ca
MJRLegends <MJRLegends@users.noreply.github.com>
2017-08-03 16:34:20 +0100

    Fixed rendering issues with Item Rocket Models

--- src/main/java/com/mjr/extraplanets/client/model/rockets/ItemModelRocketT10.java
+++ src/main/java/com/mjr/extraplanets/client/model/rockets/ItemModelRocketT10.java
@@ -8,6 +8,7 @@ import micdoodle8.mods.galacticraft.core.util.ClientUtil;
 import micdoodle8.mods.galacticraft.core.wrappers.ModelTransformWrapper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraftforge.common.model.TRSRTransformation;
@@ -21,49 +22,52 @@ public class ItemModelRocketT10 extends ModelTransformWrapper {
 		super(modelToWrap);
 	}
 
-	@Override
-	protected Matrix4f getTransformForPerspective(TransformType cameraTransformType) {
-		if (cameraTransformType == TransformType.GUI) {
-			if (Minecraft.isAmbientOcclusionEnabled()) {
-				GlStateManager.shadeModel(GL11.GL_SMOOTH);
-			} else {
-				GlStateManager.shadeModel(GL11.GL_FLAT);
-			}
-			Vector3f trans = new Vector3f(-0.12F, 0.0F, -0.12F);
-			Matrix4f ret = new Matrix4f();
-			ret.setIdentity();
-			Matrix4f mul = new Matrix4f();
-			mul.setIdentity();
-			Quat4f rot = TRSRTransformation.quatFromXYZDegrees(new Vector3f(30, 225, 0));
-			mul.setRotation(rot);
-			ret.mul(mul);
-			mul.setIdentity();
-			mul.setScale(0.25F);
-			ret.mul(mul);
-			mul.setIdentity();
-			mul.setTranslation(new Vector3f(-1.55F, -1.30F, 0.0F));
-			ret.mul(mul);
-			mul.setIdentity();
-			mul.rotY(Constants.halfPI);
-			ret.mul(mul);
-			mul.setIdentity();
-			mul.rotX((float) (Math.PI / 4.0F));
-			ret.mul(mul);
-			mul.setIdentity();
-			mul.setTranslation(trans);
-			ret.mul(mul);
-			mul.setIdentity();
-			mul.rotY(ClientUtil.getMilliseconds() / 1000.0F);
-			ret.mul(mul);
-			mul.setIdentity();
-			trans.scale(-1.0F);
-			mul.setTranslation(trans);
-			ret.mul(mul);
-			mul.setIdentity();
-			mul.setScale(0.3F);
-			ret.mul(mul);
-			return ret;
-		}
+    @Override
+    protected Matrix4f getTransformForPerspective(TransformType cameraTransformType)
+    {
+        if (cameraTransformType == TransformType.GUI)
+        {
+    		if (Minecraft.isAmbientOcclusionEnabled()) {
+    			GlStateManager.shadeModel(GL11.GL_SMOOTH);
+    		} else {
+    			GlStateManager.shadeModel(GL11.GL_FLAT);
+    		}
+    		RenderHelper.enableStandardItemLighting();
+            Vector3f trans = new Vector3f(-0.12F, 0.0F, -0.12F);
+            Matrix4f ret = new Matrix4f();
+            ret.setIdentity();
+            Matrix4f mul = new Matrix4f();
+            mul.setIdentity();
+            Quat4f rot = TRSRTransformation.quatFromXYZDegrees(new Vector3f(30, 225, 0));
+            mul.setRotation(rot);
+            ret.mul(mul);
+            mul.setIdentity();
+            mul.setScale(0.25F);
+            ret.mul(mul);
+            mul.setIdentity();
+            mul.setTranslation(new Vector3f(-1.55F, -1.30F, 0.0F));
+            ret.mul(mul);
+            mul.setIdentity();
+            mul.rotY(Constants.halfPI);
+            ret.mul(mul);
+            mul.setIdentity();
+            mul.rotX((float) (Math.PI / 4.0F));
+            ret.mul(mul);
+            mul.setIdentity();
+            mul.setTranslation(trans);
+            ret.mul(mul);
+            mul.setIdentity();
+            mul.rotY(ClientUtil.getMilliseconds() / 1000.0F);
+            ret.mul(mul);
+            mul.setIdentity();
+            trans.scale(-1.0F);
+            mul.setTranslation(trans);
+            ret.mul(mul);
+            mul.setIdentity();
+            mul.setScale(0.3F);
+            ret.mul(mul);
+            return ret;
+        }
 
 		if (cameraTransformType == TransformType.FIRST_PERSON_RIGHT_HAND) {
 			if (Minecraft.isAmbientOcclusionEnabled()) {
