23e39dca94316a48e3655c778bb2f6bc7120b3ad
MJRLegends <MJRLegends@users.noreply.github.com>
2017-08-20 18:10:40 +0100

    Added a Lander for Mercury

--- src/main/java/com/mjr/extraplanets/planets/Mercury/TeleportTypeMercury.java
+++ src/main/java/com/mjr/extraplanets/planets/Mercury/TeleportTypeMercury.java
@@ -6,12 +6,14 @@ import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.api.world.ITeleportType;
 import micdoodle8.mods.galacticraft.core.entities.player.GCCapabilities;
 import micdoodle8.mods.galacticraft.core.entities.player.GCPlayerStats;
+import micdoodle8.mods.galacticraft.core.util.CompatibilityManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 import com.mjr.extraplanets.Constants;
+import com.mjr.extraplanets.entities.landers.EntityMercuryLander;
 
 public class TeleportTypeMercury implements ITeleportType {
 	@Override
@@ -36,13 +38,30 @@ public class TeleportTypeMercury implements ITeleportType {
 
 	@Override
 	public Vector3 getParaChestSpawnLocation(WorldServer world, EntityPlayerMP player, Random rand) {
-		final double x = (rand.nextDouble() * 2 - 1.0D) * 5.0D;
-		final double z = (rand.nextDouble() * 2 - 1.0D) * 5.0D;
-		return new Vector3(player.posX + x, Constants.PLANET_AND_MOON_PARA_CHEST_SPAWN_HEIGHT_D, player.posZ + z);
+		return null;
 	}
 
 	@Override
 	public void onSpaceDimensionChanged(World newWorld, EntityPlayerMP player, boolean ridingAutoRocket) {
+		if (!ridingAutoRocket && player != null) {
+			GCPlayerStats stats = GCPlayerStats.get(player);
+
+			if (stats.getTeleportCooldown() <= 0) {
+				if (player.capabilities.isFlying) {
+					player.capabilities.isFlying = false;
+				}
+
+				if (!newWorld.isRemote) {
+					EntityMercuryLander entryPod = new EntityMercuryLander(player);
+					CompatibilityManager.forceLoadChunks((WorldServer) newWorld);
+					entryPod.forceSpawn = true;
+					newWorld.spawnEntityInWorld(entryPod);
+					CompatibilityManager.forceLoadChunksEnd((WorldServer) newWorld);
+				}
+
+				stats.setTeleportCooldown(10);
+			}
+		}
 	}
 
 	@Override
