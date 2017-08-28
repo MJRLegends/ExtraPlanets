6140bd1e0c30e8caa2552b5b7beda45bb3689921
MJRLegends <MJRLegends@users.noreply.github.com>
2017-08-28 21:40:11 +0100

    Changed the Galacticraft version enforcer for to build 117 as a minimum version to use

--- src/main/java/com/mjr/extraplanets/handlers/GalacticraftVersionChecker.java
+++ src/main/java/com/mjr/extraplanets/handlers/GalacticraftVersionChecker.java
@@ -11,16 +11,16 @@ import com.mjr.extraplanets.Constants;
 public class GalacticraftVersionChecker {
 	public static void run() {
 		try {
-			Class.forName("micdoodle8.mods.galacticraft.core.entities.EntityEvolvedEnderman");
+			Class.forName("micdoodle8.mods.galacticraft.core.energy.tile.TileCableIC2Sealed");
 		} catch (ClassNotFoundException e) {
 			Logger log = LogManager.getLogger();
-			log.fatal(Constants.modName + ": has detected you are using a version below the required minimum version of build #102, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
+			log.fatal(Constants.modName + ": has detected you are using a version below the required minimum version of build #117, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
 			JFrame frame = new JFrame();
 			frame.setSize(600, 600);
 			JOptionPane.showMessageDialog(frame, "<html><center><p><h3><font Color=red>" + Constants.modName
-					+ " has detected you are using a version below the required minimum version of build #102 </font><font Color=blue> <br> You can find it here https://micdoodle8.com/mods/galacticraft/downloads", "Incompatable error", 0);
+					+ " has detected you are using a version below the required minimum version of build #117 </font><font Color=blue> <br> You can find it here https://micdoodle8.com/mods/galacticraft/downloads", "Incompatable error", 0);
 			frame.setFocusable(true);
-			throw new RuntimeException(Constants.modName + ": has detected you are using a version below the required minimum version of build #102, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
+			throw new RuntimeException(Constants.modName + ": has detected you are using a version below the required minimum version of build #117, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
 		}
 	}
 }
