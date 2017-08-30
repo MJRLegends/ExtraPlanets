d81711e52554ae1e0b2ecad73ebaf1f47400eeb7
MJRLegends <MJRLegends@users.noreply.github.com>
2017-08-30 14:30:35 +0100

    Made Geiger Counter not stackable

--- src/main/java/com/mjr/extraplanets/items/ItemGeigerCounter.java
+++ src/main/java/com/mjr/extraplanets/items/ItemGeigerCounter.java
@@ -26,6 +26,7 @@ public class ItemGeigerCounter extends Item {
 		this.setHasSubtypes(true);
 		this.setUnlocalizedName(assetName);
 		this.setCreativeTab(ExtraPlanets.ItemsTab);
+		this.setMaxStackSize(1);
 	}
 
 	@Override
