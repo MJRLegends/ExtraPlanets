7c5260dc9138a220d63d1a797ae57b8c83762c0d
MJRLegends <MJRLegends@users.noreply.github.com>
2017-08-30 01:18:27 +0100

    Added Geiger Counter Item

---
+++ src/main/java/com/mjr/extraplanets/items/ItemGeigerCounter.java
@@ -0,0 +1,65 @@
+package com.mjr.extraplanets.items;
+
+import java.util.List;
+
+import micdoodle8.mods.galacticraft.core.util.EnumColor;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import com.mjr.extraplanets.ExtraPlanets;
+import com.mjr.extraplanets.handlers.capabilities.CapabilityStatsHandler;
+import com.mjr.extraplanets.handlers.capabilities.IStatsCapability;
+
+public class ItemGeigerCounter extends Item {
+	public ItemGeigerCounter(String assetName) {
+		super();
+		this.setMaxDamage(0);
+		this.setHasSubtypes(true);
+		this.setUnlocalizedName(assetName);
+		this.setCreativeTab(ExtraPlanets.ItemsTab);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List<String> list, boolean par4) {
+		if (player.worldObj.isRemote) {
+			list.add(EnumColor.YELLOW + GCCoreUtil.translate("geiger.counter.desc"));
+		}
+	}
+
+	@Override
+	public EnumAction getItemUseAction(ItemStack stack) {
+		return EnumAction.NONE;
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		playerIn.setActiveHand(hand);
+		if (playerIn.worldObj.isRemote == false) {
+			EntityPlayerMP playerMP = (EntityPlayerMP) playerIn;
+			IStatsCapability stats = null;
+			if (playerMP != null) {
+				stats = playerMP.getCapability(CapabilityStatsHandler.EP_STATS_CAPABILITY, null);
+			}
+			playerIn.addChatMessage(new TextComponentString("" + TextFormatting.AQUA + TextFormatting.BOLD + playerMP.getName() + TextFormatting.DARK_AQUA + ", Your Current Radiation Level is: " + (int) stats.getRadiationLevel() + "%"));
+		}
+		return new ActionResult(EnumActionResult.SUCCESS, itemStackIn);
+	}
+
+	@Override
+	public int getItemStackLimit(ItemStack stack) {
+		return 1;
+	}
+}
