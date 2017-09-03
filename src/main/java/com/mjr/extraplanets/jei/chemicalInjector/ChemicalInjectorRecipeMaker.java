f8f5f2e527b9c71c4fbf3922e4392f72d7197fff
MJRLegends <MJRLegends@users.noreply.github.com>
2017-09-03 01:33:54 +0100

    Fixed machine issues with using the wrong amount of materials

--- src/main/java/com/mjr/extraplanets/jei/chemicalInjector/ChemicalInjectorRecipeMaker.java
+++ src/main/java/com/mjr/extraplanets/jei/chemicalInjector/ChemicalInjectorRecipeMaker.java
@@ -11,8 +11,8 @@ public class ChemicalInjectorRecipeMaker {
 	public static List<ChemicalInjectorRecipeWrapper> getRecipesList() {
 		List<ChemicalInjectorRecipeWrapper> recipes = new ArrayList<>();
 		ItemStack[] inputs = new ItemStack[2];
-		inputs[0] = (new ItemStack(ExtraPlanets_Items.IODIDE_SALT));
-		inputs[1] = (new ItemStack(ExtraPlanets_Items.POTASSIUM));
+		inputs[0] = (new ItemStack(ExtraPlanets_Items.IODIDE_SALT, 3, 0));
+		inputs[1] = (new ItemStack(ExtraPlanets_Items.POTASSIUM, 6, 0));
 		ChemicalInjectorRecipeWrapper wrapper = new ChemicalInjectorRecipeWrapper(inputs, new ItemStack(ExtraPlanets_Items.POTASSIUM_IODIDE, 1, 0));
 		recipes.add(wrapper);
 
