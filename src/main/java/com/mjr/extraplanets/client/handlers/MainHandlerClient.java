75f59f2a44ae63c504300689d12edef3331a46b1
MJRLegends <MJRLegends@users.noreply.github.com>
2017-10-21 01:12:27 +0100

    Added tooltip for hints to where you can find each fluid in ExtraPlanets

--- src/main/java/com/mjr/extraplanets/client/handlers/MainHandlerClient.java
+++ src/main/java/com/mjr/extraplanets/client/handlers/MainHandlerClient.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
 import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
 import micdoodle8.mods.galacticraft.core.util.ConfigManagerCore;
+import micdoodle8.mods.galacticraft.core.util.EnumColor;
+import micdoodle8.mods.galacticraft.core.util.FluidUtil;
 import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
 import micdoodle8.mods.galacticraft.core.util.OxygenUtil;
 import micdoodle8.mods.galacticraft.core.util.PlayerUtil;
@@ -15,8 +17,13 @@ import micdoodle8.mods.galacticraft.planets.venus.client.FakeLightningBoltRender
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.UniversalBucket;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -30,6 +37,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.mjr.extraplanets.Config;
 import com.mjr.extraplanets.ExtraPlanets;
+import com.mjr.extraplanets.blocks.fluid.ExtraPlanets_Fluids;
 import com.mjr.extraplanets.client.gui.overlay.OverlayElectricLaunchCountdown;
 import com.mjr.extraplanets.client.gui.overlay.OverlayJupiterLander;
 import com.mjr.extraplanets.client.gui.overlay.OverlayMercuryLander;
@@ -125,7 +133,7 @@ public class MainHandlerClient {
 		}
 		if (minecraft.currentScreen == null && player.ridingEntity instanceof EntityJupiterLander && minecraft.gameSettings.thirdPersonView != 0 && !minecraft.gameSettings.hideGUI) {
 			OverlayJupiterLander.renderLanderOverlay();
-		}		
+		}
 		if (minecraft.currentScreen == null && player.ridingEntity instanceof EntitySaturnLander && minecraft.gameSettings.thirdPersonView != 0 && !minecraft.gameSettings.hideGUI) {
 			OverlaySaturnLander.renderLanderOverlay();
 		}
@@ -134,7 +142,7 @@ public class MainHandlerClient {
 		}
 		if (minecraft.currentScreen == null && player.ridingEntity instanceof EntityUranusLander && minecraft.gameSettings.thirdPersonView != 0 && !minecraft.gameSettings.hideGUI) {
 			OverlayUranusLander.renderLanderOverlay();
-		}		
+		}
 		if (minecraft.currentScreen == null && player.ridingEntity instanceof EntityNeptuneLander && minecraft.gameSettings.thirdPersonView != 0 && !minecraft.gameSettings.hideGUI) {
 			OverlayNeptuneLander.renderLanderOverlay();
 		}
@@ -181,9 +189,27 @@ public class MainHandlerClient {
 					double posX = player.posX + dX;
 					double posY = 70;
 					double posZ = player.posZ + dZ;
-                    minecraft.theWorld.playSound(posX, posY, posZ, "ambient.weather.thunder", 1000.0F, 1.0F + player.getRNG().nextFloat() * 0.2F, false);
+					minecraft.theWorld.playSound(posX, posY, posZ, "ambient.weather.thunder", 1000.0F, 1.0F + player.getRNG().nextFloat() * 0.2F, false);
 					lightning.put(new BlockPos(posX, posY, posZ), 20);
 				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onToolTip(ItemTooltipEvent event) {
+		ItemStack stack = event.itemStack;
+		if (stack != null && stack.getItem() != null && stack.getItem() instanceof UniversalBucket) {
+			if (FluidUtil.getFluidContained(stack) != null && FluidUtil.getFluidContained(stack).getFluid() != null) {
+				FluidStack fluidStack = FluidUtil.getFluidContained(stack);
+				Fluid fluid = fluidStack.getFluid();
+				if (fluid.equals(ExtraPlanets_Fluids.FROZEN_WATER_FLUID) || fluid.equals(ExtraPlanets_Fluids.GLOWSTONE_FLUID) || fluid.equals(ExtraPlanets_Fluids.INFECTED_WATER_FLUID) || fluid.equals(ExtraPlanets_Fluids.LIQUID_HYDROCARBON_FLUID)
+						|| fluid.equals(ExtraPlanets_Fluids.MAGMA_FLUID) || fluid.equals(ExtraPlanets_Fluids.METHANE_FLUID) || fluid.equals(ExtraPlanets_Fluids.NITROGEN_FLUID) || fluid.equals(ExtraPlanets_Fluids.RADIO_ACTIVE_WATER_FLUID)
+						|| fluid.equals(ExtraPlanets_Fluids.SALT_FLUID)) {
+					event.toolTip.add(EnumColor.AQUA + GCCoreUtil.translate("gui.bucket.message.finding"));
+					event.toolTip.add(EnumColor.AQUA + GCCoreUtil.translate("gui.bucket.message.finding.2"));
+				} else if (fluid.equals(ExtraPlanets_Fluids.CLEAN_WATER_FLUID))
+					event.toolTip.add(EnumColor.ORANGE + GCCoreUtil.translate("gui.bucket.message.crafting"));
 			}
 		}
 	}
