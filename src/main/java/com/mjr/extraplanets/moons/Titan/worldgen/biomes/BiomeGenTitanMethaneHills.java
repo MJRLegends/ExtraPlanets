af6155f8a4e76928c5f52f8d7cd0cbdbb0a0ecab
MJRLegends <MJRLegends@users.noreply.github.com>
2017-11-27 03:12:43 +0000

    Fixes for MC 1.11.2

--- src/main/java/com/mjr/extraplanets/moons/Titan/worldgen/biomes/BiomeGenTitanMethaneHills.java
+++ src/main/java/com/mjr/extraplanets/moons/Titan/worldgen/biomes/BiomeGenTitanMethaneHills.java
@@ -1,14 +1,18 @@
 package com.mjr.extraplanets.moons.Titan.worldgen.biomes;
 
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
 
+import com.mjr.extraplanets.Config;
+import com.mjr.extraplanets.Constants;
 import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
 import com.mjr.extraplanets.moons.Titan.worldgen.TitanBiomes;
 
 public class BiomeGenTitanMethaneHills extends TitanBiomes {
 	public BiomeGenTitanMethaneHills(BiomeProperties properties) {
 		super(properties);
-		BiomeDictionary.registerBiomeType(this, BiomeDictionary.Type.COLD, BiomeDictionary.Type.DRY, BiomeDictionary.Type.DEAD, BiomeDictionary.Type.SANDY);
+		Biome.registerBiome(Config.TITAN_METHANE_HILLS_BIOME_ID, Constants.TEXTURE_PREFIX + this.getBiomeName(), this);
+		BiomeDictionary.addTypes(this, BiomeDictionary.Type.COLD, BiomeDictionary.Type.DRY, BiomeDictionary.Type.DEAD, BiomeDictionary.Type.SANDY);
 		this.topBlock = ExtraPlanets_Blocks.TITAN_BLOCKS.getStateFromMeta(6);
 		this.fillerBlock = ExtraPlanets_Blocks.TITAN_BLOCKS.getStateFromMeta(7);
 	}
