2241d99058f78bdcdd6b58ad75b77cbaca047cbc
MJRLegends <MJRLegends@users.noreply.github.com>
2017-11-29 14:07:44 +0000

    Clean up

--- src/main/java/com/mjr/extraplanets/moons/Titan/worldgen/TitanBiomes.java
+++ src/main/java/com/mjr/extraplanets/moons/Titan/worldgen/TitanBiomes.java
@@ -1,6 +1,5 @@
 package com.mjr.extraplanets.moons.Titan.worldgen;
 
-import java.util.List;
 import java.util.Random;
 
 import micdoodle8.mods.galacticraft.core.entities.EntityEvolvedCreeper;
@@ -9,7 +8,6 @@ import micdoodle8.mods.galacticraft.core.entities.EntityEvolvedSpider;
 import micdoodle8.mods.galacticraft.core.entities.EntityEvolvedZombie;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
