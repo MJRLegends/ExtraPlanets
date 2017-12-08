8dca35661a341dc4611c52f28695dc204e0b56c0
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-08 15:11:11 +0000

    *Small amount of fixing of some deprecated code by changing it to the new method

--- src/main/java/com/mjr/extraplanets/moons/Io/worldgen/biomes/BiomeGenIo.java
+++ src/main/java/com/mjr/extraplanets/moons/Io/worldgen/biomes/BiomeGenIo.java
@@ -6,6 +6,7 @@ import net.minecraftforge.common.BiomeDictionary;
 import com.mjr.extraplanets.Config;
 import com.mjr.extraplanets.Constants;
 import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
+import com.mjr.extraplanets.blocks.planetAndMoonBlocks.BlockBasicIo;
 import com.mjr.extraplanets.moons.Io.worldgen.IoBiomes;
 
 public class BiomeGenIo extends IoBiomes {
@@ -15,6 +16,6 @@ public class BiomeGenIo extends IoBiomes {
 		Biome.registerBiome(Config.IO_BIOME_ID, Constants.TEXTURE_PREFIX + this.getBiomeName(), this);
 		BiomeDictionary.addTypes(this, BiomeDictionary.Type.COLD, BiomeDictionary.Type.DRY, BiomeDictionary.Type.DEAD, BiomeDictionary.Type.SANDY);
 		this.topBlock = ExtraPlanets_Blocks.IO_BLOCKS.getDefaultState();
-		this.fillerBlock = ExtraPlanets_Blocks.IO_BLOCKS.getStateFromMeta(1);
+		this.fillerBlock = ExtraPlanets_Blocks.IO_BLOCKS.getDefaultState().withProperty(BlockBasicIo.BASIC_TYPE, BlockBasicIo.EnumBlockBasic.SUB_SURFACE);
 	}
 }
