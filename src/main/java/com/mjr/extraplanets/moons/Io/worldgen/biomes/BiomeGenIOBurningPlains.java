e3dd49c70a8bbd4233ea5133dc8a5adb11ec3fe0
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-08 00:36:52 +0000

    Fixes for MC 1.11.2

--- src/main/java/com/mjr/extraplanets/moons/Io/worldgen/biomes/BiomeGenIOBurningPlains.java
+++ src/main/java/com/mjr/extraplanets/moons/Io/worldgen/biomes/BiomeGenIOBurningPlains.java
@@ -1,7 +1,10 @@
 package com.mjr.extraplanets.moons.Io.worldgen.biomes;
 
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
 
+import com.mjr.extraplanets.Config;
+import com.mjr.extraplanets.Constants;
 import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
 import com.mjr.extraplanets.moons.Io.worldgen.IoBiomes;
 
@@ -9,7 +12,8 @@ public class BiomeGenIOBurningPlains extends IoBiomes {
 
 	public BiomeGenIOBurningPlains(BiomeProperties properties) {
 		super(properties);
-		BiomeDictionary.registerBiomeType(this, BiomeDictionary.Type.HOT, BiomeDictionary.Type.DRY, BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.SPOOKY);
+		Biome.registerBiome(Config.IO_BURNING_PLAINS_BIOME_ID, Constants.TEXTURE_PREFIX + this.getBiomeName(), this);
+		BiomeDictionary.addTypes(this, BiomeDictionary.Type.HOT, BiomeDictionary.Type.DRY, BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.SPOOKY);
 		this.topBlock = ExtraPlanets_Blocks.VOLCANIC_ROCK.getDefaultState();
 		this.fillerBlock = ExtraPlanets_Blocks.ASH_ROCK.getDefaultState();
 	}
