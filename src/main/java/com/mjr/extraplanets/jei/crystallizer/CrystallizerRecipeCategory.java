c6822f13c463dd72abdc37ff8659709136b32923
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-11 01:49:43 +0000

    Fixed JEI support warning messages

--- src/main/java/com/mjr/extraplanets/jei/crystallizer/CrystallizerRecipeCategory.java
+++ src/main/java/com/mjr/extraplanets/jei/crystallizer/CrystallizerRecipeCategory.java
@@ -64,7 +64,7 @@ public class CrystallizerRecipeCategory extends BlankRecipeCategory {
 		IGuiItemStackGroup itemstacks = recipeLayout.getItemStacks();
 
 		itemstacks.init(0, true, 3, 2);
-		itemstacks.init(2, false, 108, 30);
+		itemstacks.init(1, false, 108, 30);
 		itemstacks.set(ingredients);
 	}
 
