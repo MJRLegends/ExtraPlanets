8f164908712cdd1a5c525ad40005d2a8cd467ffe
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-12 01:08:02 +0000

    Fixed issue #180

--- src/main/java/com/mjr/extraplanets/client/gui/rockets/GuiSchematicTier7Rocket.java
+++ src/main/java/com/mjr/extraplanets/client/gui/rockets/GuiSchematicTier7Rocket.java
@@ -54,7 +54,7 @@ public class GuiSchematicTier7Rocket extends GuiContainerGC implements ISchemati
 	@Override
 	protected void drawGuiContainerForegroundLayer(int par1, int par2)
 	{
-		this.fontRendererObj.drawString(GCCoreUtil.translate("schematic.rocketT7.name"), 7, -20 + 27, 4210752);
+		this.fontRendererObj.drawString(GCCoreUtil.translate("schematic.rocket.t7.name"), 7, -20 + 27, 4210752);
 		this.fontRendererObj.drawString(GCCoreUtil.translate("container.inventory"), 8, 220 - 104 + 2 + 27, 4210752);
 	}
 
--- src/main/java/com/mjr/extraplanets/client/gui/vehicles/GuiSchematicVenusRover.java
+++ src/main/java/com/mjr/extraplanets/client/gui/vehicles/GuiSchematicVenusRover.java
@@ -48,7 +48,7 @@ public class GuiSchematicVenusRover extends GuiContainerGC implements ISchematic
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(int par1, int par2) {
-		this.fontRendererObj.drawString(GCCoreUtil.translate("schematic.venusRover.name"), 7, -20 + 27, 4210752);
+		this.fontRendererObj.drawString(GCCoreUtil.translate("schematic.venus.rover.name"), 7, -20 + 27, 4210752);
 		this.fontRendererObj.drawString(GCCoreUtil.translate("container.inventory"), 8, 202 - 104 + 2 + 27, 4210752);
 	}
 
