376c3b572520d5d945717d27be834c4bbbcf0330
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-18 16:40:35 +0000

    Changes to Jupiter Sky Provider

--- src/main/java/com/mjr/extraplanets/planets/Jupiter/SkyProviderJupiter.java
+++ src/main/java/com/mjr/extraplanets/planets/Jupiter/SkyProviderJupiter.java
@@ -23,7 +23,8 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
 public class SkyProviderJupiter extends IRenderHandler {
-	private static final ResourceLocation overworldTexture = new ResourceLocation(Constants.ASSET_PREFIX, "textures/gui/celestialbodies/saturn.png");
+	private static final ResourceLocation overworldTexture = new ResourceLocation(com.mjr.extraplanets.Constants.ASSET_PREFIX, "textures/gui/celestialbodies/ceres.png");
+	private static final ResourceLocation overworldTexture2 = new ResourceLocation(Constants.ASSET_PREFIX, "textures/gui/celestialbodies/saturn.png");
 	private static final ResourceLocation sunTexture = new ResourceLocation(Constants.ASSET_PREFIX, "textures/gui/planets/atmosphericsun.png");
 
 	public int starList;
@@ -239,12 +240,26 @@ public class SkyProviderJupiter extends IRenderHandler {
 		tessellator1.draw();
 
 		// Render earth
-		f10 = 0.5F;
+		f10 = 2.5F;
 		GL11.glScalef(0.6F, 0.6F, 0.6F);
 		GL11.glRotatef(40.0F, 0.0F, 0.0F, 1.0F);
 		GL11.glRotatef(200F, 1.0F, 0.0F, 0.0F);
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1F);
+		GL11.glColor4f(0.5F, 0.5F, 0.5F, 1.0F);
 		FMLClientHandler.instance().getClient().renderEngine.bindTexture(SkyProviderJupiter.overworldTexture);
+		worldRenderer1.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+		worldRenderer1.pos(-f10, -100.0D, f10).tex(0, 1).endVertex();
+		worldRenderer1.pos(f10, -100.0D, f10).tex(1, 1).endVertex();
+		worldRenderer1.pos(f10, -100.0D, -f10).tex(1, 0).endVertex();
+		worldRenderer1.pos(-f10, -100.0D, -f10).tex(0, 0).endVertex();
+		tessellator1.draw();
+		
+		// Render earth
+		f10 = 4.5F;
+		GL11.glScalef(0.6F, 0.6F, 0.6F);
+		GL11.glRotatef(40.0F, 0.0F, 0.0F, 1.0F);
+		GL11.glRotatef(200F, 1.0F, 0.0F, 0.0F);
+		GL11.glColor4f(0.6F, 0.6F, 0.6F, 1F);
+		FMLClientHandler.instance().getClient().renderEngine.bindTexture(SkyProviderJupiter.overworldTexture2);
 		worldRenderer1.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
 		worldRenderer1.pos(-f10, -100.0D, f10).tex(0, 1).endVertex();
 		worldRenderer1.pos(f10, -100.0D, f10).tex(1, 1).endVertex();
