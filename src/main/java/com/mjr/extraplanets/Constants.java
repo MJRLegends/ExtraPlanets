fc3e3773dd7b6fbd31de13da480fcabca67d437c
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-19 01:41:40 +0000

    Changed some math over to using Constants variables

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -22,6 +22,7 @@ public class Constants {
 
 	public static final float twoPI = Constants.floatPI * 2F;
 	public static final float halfPI = Constants.floatPI / 2F;
+	public static final float quarterPI = Constants.halfPI / 2F;
 	public static final float floatPI = 3.1415927F;
 	
 	public static final int SPACE_STATION_LOWER_Y_LIMIT = 10;
