cc08ef2b2f5ff2e374d806219d1614d73e83acc4
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-19 01:19:56 +0000

    *Changes to Ceres Sky Provider

--- src/main/java/com/mjr/extraplanets/planets/Ceres/SkyProviderCeres.java
+++ src/main/java/com/mjr/extraplanets/planets/Ceres/SkyProviderCeres.java
@@ -158,7 +158,7 @@ public class SkyProviderCeres extends IRenderHandler {
 		tessellator1.setColorRGBA_F(afloat[0] * f18, afloat[1] * f18, afloat[2] * f18, 0.0F);
 
 		// Render sun aura
-		f10 = 20.0F;
+		f10 = 5.0F;
 		tessellator1.addVertex(-f10, 100.0D, -f10);
 		tessellator1.addVertex(0, 100.0D, (double) -f10 * 1.5F);
 		tessellator1.addVertex(f10, 100.0D, -f10);
@@ -223,12 +223,12 @@ public class SkyProviderCeres extends IRenderHandler {
 		tessellator1.draw();
 
 		// Render earth
-		f10 = 5.5F;
+		f10 = 2.5F;
 		GL11.glScalef(0.6F, 0.6F, 0.6F);
 		GL11.glRotatef(40.0F, 0.0F, 0.0F, 1.0F);
 		GL11.glRotatef(200F, 1.0F, 0.0F, 0.0F);
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1F);
-		FMLClientHandler.instance().getClient().renderEngine.bindTexture(SkyProviderCeres.overworldTexture2);
+		FMLClientHandler.instance().getClient().renderEngine.bindTexture(SkyProviderCeres.overworldTexture);
 		tessellator1.startDrawingQuads();
 		tessellator1.addVertexWithUV(-f10, -100.0D, f10, 0, 1);
 		tessellator1.addVertexWithUV(f10, -100.0D, f10, 1, 1);
@@ -239,12 +239,12 @@ public class SkyProviderCeres extends IRenderHandler {
 		GL11.glRotatef(-20.0F, 1.0F, 0.0F, 0.0F);
 
 		// Render earth
-		f10 = 2.5F;
+		f10 = 5.5F;
 		GL11.glScalef(0.6F, 0.6F, 0.6F);
 		GL11.glRotatef(40.0F, 0.0F, 0.0F, 1.0F);
 		GL11.glRotatef(200F, 1.0F, 0.0F, 0.0F);
 		GL11.glColor4f(0.7F, 0.5F, 0.5F, 0.6F);
-		FMLClientHandler.instance().getClient().renderEngine.bindTexture(SkyProviderCeres.overworldTexture);
+		FMLClientHandler.instance().getClient().renderEngine.bindTexture(SkyProviderCeres.overworldTexture2);
 		tessellator1.startDrawingQuads();
 		tessellator1.addVertexWithUV(-f10, -100.0D, f10, 0, 1);
 		tessellator1.addVertexWithUV(f10, -100.0D, f10, 1, 1);
