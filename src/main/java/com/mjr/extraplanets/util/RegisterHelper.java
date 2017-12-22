e42e6dedef515d1c6ccd0d37c3e25df33359645f
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-22 20:53:35 +0000

    Refactoring some methods

--- src/main/java/com/mjr/extraplanets/util/RegisterHelper.java
+++ src/main/java/com/mjr/extraplanets/util/RegisterHelper.java
@@ -57,12 +57,12 @@ public class RegisterHelper {
 		GameRegistry.register(item.setRegistryName(name));
 	}
 
-	public static void registerExtraPlanetsNonMobEntity(Class<? extends Entity> var0, String var1, int trackingDistance, int updateFreq, boolean sendVel) {
+	public static void registerNonMobEntity(Class<? extends Entity> var0, String var1, int trackingDistance, int updateFreq, boolean sendVel) {
 		EntityRegistry.registerModEntity(var0, var1, id++, ExtraPlanets.instance, trackingDistance, updateFreq, sendVel);
 	}
 	
-	public static void registerExtraPlanetsMobEntity(Class<? extends Entity> entityClass, String name, int back, int fore) {
-		registerExtraPlanetsNonMobEntity(entityClass, name, 80, 3, true);
+	public static void registerMobEntity(Class<? extends Entity> entityClass, String name, int back, int fore) {
+		registerNonMobEntity(entityClass, name, 80, 3, true);
 		EntityRegistry.registerEgg(entityClass, back, fore);
 	}
 	
