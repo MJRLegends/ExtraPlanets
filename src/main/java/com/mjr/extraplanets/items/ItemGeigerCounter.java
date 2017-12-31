1670cf2dbd865d54a6f166b9234392598a666a45
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-31 01:51:14 +0000

    *More converting of ingame texture to have lang file support *Added missing lang entries for new features

--- src/main/java/com/mjr/extraplanets/items/ItemGeigerCounter.java
+++ src/main/java/com/mjr/extraplanets/items/ItemGeigerCounter.java
@@ -56,7 +56,7 @@ public class ItemGeigerCounter extends Item {
 			if (playerMP != null) {
 				stats = playerMP.getCapability(CapabilityStatsHandler.EP_STATS_CAPABILITY, null);
 			}
-			player.sendMessage(new TextComponentString("" + TextFormatting.AQUA + TextFormatting.BOLD + playerMP.getName() + TextFormatting.DARK_AQUA + ", Your Current Radiation Level is: " + (int) stats.getRadiationLevel() + "%"));
+			playerIn.sendMessage(new TextComponentString("" + TextFormatting.AQUA + TextFormatting.BOLD + playerMP.getName() + TextFormatting.DARK_AQUA + ", " + GCCoreUtil.translate("gui.radiation.current.message") + ": " + (int) stats.getRadiationLevel() + "%"));
 		}
 		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemStackIn);
 	}
