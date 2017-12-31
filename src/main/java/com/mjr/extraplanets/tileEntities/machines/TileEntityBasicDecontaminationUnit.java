ebfe21b6861eb19fd11fd8a452b9315e69f69348
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-31 02:03:09 +0000

    Fixes for Basic Decontamination Unit GUI/Missing GUI Texture

--- src/main/java/com/mjr/extraplanets/tileEntities/machines/TileEntityBasicDecontaminationUnit.java
+++ src/main/java/com/mjr/extraplanets/tileEntities/machines/TileEntityBasicDecontaminationUnit.java
@@ -46,7 +46,7 @@ public class TileEntityBasicDecontaminationUnit extends TileBaseElectricBlock im
 	public TileEntityBasicDecontaminationUnit() {
 		super();
 		this.storage.setCapacity(1000000);
-		this.storage.setMaxExtract(1000000);
+		this.storage.setMaxExtract(500);
 	}
 
 	@Override
