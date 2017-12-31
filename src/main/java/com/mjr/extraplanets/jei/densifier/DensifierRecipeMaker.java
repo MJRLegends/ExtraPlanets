0445d637c12d6ac0e598d74786bb088e502ac9fa
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-31 02:12:28 +0000

    Added JEI Support for new Densifier recipes

--- src/main/java/com/mjr/extraplanets/jei/densifier/DensifierRecipeMaker.java
+++ src/main/java/com/mjr/extraplanets/jei/densifier/DensifierRecipeMaker.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.ForgeModContainer;
 import net.minecraftforge.fluids.UniversalBucket;
 
+import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
 import com.mjr.extraplanets.blocks.fluid.ExtraPlanets_Fluids;
 import com.mjr.extraplanets.items.ExtraPlanets_Items;
 
@@ -27,6 +29,26 @@ public class DensifierRecipeMaker {
 		inputs = new ItemStack[1];
 		inputs[0] = UniversalBucket.getFilledBucket(ForgeModContainer.getInstance().universalBucket, ExtraPlanets_Fluids.NITROGEN_ICE_FLUID);
 		wrapper = new DensifierRecipeWrapper(inputs, new ItemStack(Blocks.ICE, 6, 0));
+		recipes.add(wrapper);
+		
+		inputs = new ItemStack[1];
+		inputs[0] = UniversalBucket.getFilledBucket(ForgeModContainer.getInstance().universalBucket, ExtraPlanets_Fluids.GLOWSTONE_FLUID);
+		wrapper = new DensifierRecipeWrapper(inputs, new ItemStack(Blocks.GLOWSTONE, 1, 0));
+		recipes.add(wrapper);
+		
+		inputs = new ItemStack[1];
+		inputs[0] = UniversalBucket.getFilledBucket(ForgeModContainer.getInstance().universalBucket, ExtraPlanets_Fluids.NITROGEN_FLUID);
+		wrapper = new DensifierRecipeWrapper(inputs, new ItemStack(ExtraPlanets_Blocks.FROZEN_NITROGEN, 2, 0));
+		recipes.add(wrapper);
+		
+		inputs = new ItemStack[1];
+		inputs[0] = UniversalBucket.getFilledBucket(ForgeModContainer.getInstance().universalBucket, ExtraPlanets_Fluids.FROZEN_WATER_FLUID);
+		wrapper = new DensifierRecipeWrapper(inputs, new ItemStack(Blocks.ICE, 3, 0));
+		recipes.add(wrapper);
+		
+		inputs = new ItemStack[1];
+		inputs[0] = UniversalBucket.getFilledBucket(ForgeModContainer.getInstance().universalBucket, ExtraPlanets_Fluids.LIQUID_HYDROCARBON_FLUID);
+		wrapper = new DensifierRecipeWrapper(inputs, new ItemStack(Items.COAL, 3, 0));
 		recipes.add(wrapper);
 		return recipes;
 	}
