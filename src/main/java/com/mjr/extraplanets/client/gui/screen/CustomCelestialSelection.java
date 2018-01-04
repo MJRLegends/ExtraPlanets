1a99485c0cc1146bd92db652680aef0bf4280553
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-04 20:50:44 +0000

    Few fixes

--- src/main/java/com/mjr/extraplanets/client/gui/screen/CustomCelestialSelection.java
+++ src/main/java/com/mjr/extraplanets/client/gui/screen/CustomCelestialSelection.java
@@ -588,10 +588,10 @@ public class CustomCelestialSelection extends GuiCelestialSelection {
 
 					String name = this.selectedBody.getUnlocalizedName().toLowerCase();
 					String hasDungeon = "Unknown";
-					if (name.contains("moon") || name.contains("venus") || name.contains("mars") || name.contains("asteroids") || name.contains("mercury") || name.contains("jupiter") || name.contains("saturn") || name.contains("uranus")
+					if (name.contains("moon") || name.contains("venus") || name.contains("mars") || name.contains("mercury") || name.contains("jupiter") || name.contains("saturn") || name.contains("uranus")
 							|| name.contains("neptune") || name.contains("pluto") || name.contains("eris"))
 						hasDungeon = "true";
-					if (name.contains("overworld") || name.contains("ceres") || name.contains("kepler22b") || name.contains("phobos") || name.contains("deimos") || name.contains("io") || name.contains("europa") || name.contains("ganymede")
+					if (name.contains("overworld") || name.contains("ceres") || name.contains("kepler22b") || name.contains("asteroids") || name.contains("phobos") || name.contains("deimos") || name.contains("io") || name.contains("europa") || name.contains("ganymede")
 							|| name.contains("callisto") || name.contains("rhea") || name.contains("titan") || name.contains("iapetus") || name.contains("titania") || name.contains("oberon") || name.contains("triton"))
 						hasDungeon = "false";
 					this.drawString(this.fontRenderer, "Has Dungeon: " + hasDungeon, xOffset + 10, yOffset + 32, 14737632);
