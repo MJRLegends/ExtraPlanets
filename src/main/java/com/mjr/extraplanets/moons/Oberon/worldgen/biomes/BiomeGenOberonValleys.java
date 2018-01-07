7eef25be54fbd58eb4168936fd673cc2bc065bae
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-07 20:33:08 +0000

    Fixed more missed out biome registering from a few commits ago

--- src/main/java/com/mjr/extraplanets/moons/Oberon/worldgen/biomes/BiomeGenOberonValleys.java
+++ src/main/java/com/mjr/extraplanets/moons/Oberon/worldgen/biomes/BiomeGenOberonValleys.java
@@ -1,10 +1,7 @@
 package com.mjr.extraplanets.moons.Oberon.worldgen.biomes;
 
-import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
 
-import com.mjr.extraplanets.Config;
-import com.mjr.extraplanets.Constants;
 import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
 import com.mjr.extraplanets.blocks.planetAndMoonBlocks.BlockBasicOberon;
 import com.mjr.extraplanets.moons.Oberon.worldgen.OberonBiomes;
@@ -13,9 +10,11 @@ public class BiomeGenOberonValleys extends OberonBiomes {
 
 	public BiomeGenOberonValleys(BiomeProperties properties) {
 		super(properties);
-		Biome.registerBiome(Config.OBERON_VALLEYS_BIOME_ID, Constants.TEXTURE_PREFIX + this.getBiomeName(), this);
-		BiomeDictionary.addTypes(this, BiomeDictionary.Type.COLD, BiomeDictionary.Type.DRY, BiomeDictionary.Type.DEAD, BiomeDictionary.Type.SANDY);
 		this.topBlock = ExtraPlanets_Blocks.OBERON_BLOCKS.getDefaultState().withProperty(BlockBasicOberon.BASIC_TYPE, BlockBasicOberon.EnumBlockBasic.SUB_SURFACE);
 		this.fillerBlock = ExtraPlanets_Blocks.OBERON_BLOCKS.getDefaultState().withProperty(BlockBasicOberon.BASIC_TYPE, BlockBasicOberon.EnumBlockBasic.STONE);
+	}
+
+	public void registerTypes() {
+		BiomeDictionary.addTypes(this, BiomeDictionary.Type.COLD, BiomeDictionary.Type.DRY, BiomeDictionary.Type.DEAD, BiomeDictionary.Type.SANDY);
 	}
 }
