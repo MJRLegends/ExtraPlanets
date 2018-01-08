e7b1f07025a1d8a54fb83c1da7b11e953d7c7e1e
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-08 23:24:51 +0000

    Changed requirement of MJRLegendsLib to 1.11.2-1.0.2 & above

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 
 	public static final String MCVERSION = "[1.11.2]";
 	public static final String DEPENDENCIES_FORGE = "required-after:forge@[13.20.1.2513,);";
-	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib; required-after:galacticraftcore; required-after:galacticraftplanets;";
+	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.11.2-1.0.2,); required-after:galacticraftcore; required-after:galacticraftplanets;";
 
 	public static final String ASSET_PREFIX = modID;
 	public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
