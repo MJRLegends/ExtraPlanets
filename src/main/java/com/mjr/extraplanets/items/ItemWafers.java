bbc812147e0263344fc9744539416f2d6cbded67
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-10 00:18:30 +0000

    *Added Tool tips for Custom Wafers to tell the player where they are used *Updated lang files

--- src/main/java/com/mjr/extraplanets/items/ItemWafers.java
+++ src/main/java/com/mjr/extraplanets/items/ItemWafers.java
@@ -3,7 +3,9 @@ package com.mjr.extraplanets.items;
 import java.util.List;
 
 import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
+import micdoodle8.mods.galacticraft.core.util.EnumColor;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -11,6 +13,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import com.mjr.extraplanets.ExtraPlanets;
+import com.mjr.mjrlegendslib.util.TranslateUtilities;
 
 public class ItemWafers extends Item {
 	public static final String[] names = { "diamond_wafer", "carbon_wafer", "titanium_wafer", "red_gem_wafer", "blue_gem_wafer", "white_gem_wafer" };
@@ -23,6 +26,19 @@ public class ItemWafers extends Item {
 		this.setCreativeTab(ExtraPlanets.ItemsTab);
 	}
 
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List<String> list, boolean par4) {
+		if (player.worldObj.isRemote) {
+			if (itemStack != null){
+				if(itemStack.getItemDamage() == 0 || itemStack.getItemDamage() == 1)
+					list.add(EnumColor.YELLOW + TranslateUtilities.translate("currently.unused.name"));
+				else
+					list.add(EnumColor.AQUA + TranslateUtilities.translate("wafter.uses.information"));
+			}
+		}
+	}
+	
 	@Override
 	@SideOnly(Side.CLIENT)
 	public EnumRarity getRarity(ItemStack par1ItemStack) {
