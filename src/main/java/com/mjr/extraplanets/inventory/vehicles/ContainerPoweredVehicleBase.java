8fff1adc5b15c876e47bd1a1e31686b82a882085
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-14 22:26:01 +0000

    Fixed crash with shift clicking in custom vehicles

--- src/main/java/com/mjr/extraplanets/inventory/vehicles/ContainerPoweredVehicleBase.java
+++ src/main/java/com/mjr/extraplanets/inventory/vehicles/ContainerPoweredVehicleBase.java
@@ -64,7 +64,7 @@ public class ContainerPoweredVehicleBase extends Container {
 			}
 
 			if (var5.getCount() == 0) {
-				var4.putStack((ItemStack) null);
+				var4.putStack(ItemStack.EMPTY);
 			} else {
 				var4.onSlotChanged();
 			}
