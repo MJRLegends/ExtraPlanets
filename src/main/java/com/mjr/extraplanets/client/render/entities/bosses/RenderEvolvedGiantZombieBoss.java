686ca02b32db03074098936930801676a5d27a66
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-19 03:16:14 +0000

    Fixes for MC 1.12.2

--- src/main/java/com/mjr/extraplanets/client/render/entities/bosses/RenderEvolvedGiantZombieBoss.java
+++ src/main/java/com/mjr/extraplanets/client/render/entities/bosses/RenderEvolvedGiantZombieBoss.java
@@ -1,41 +1,22 @@
 package com.mjr.extraplanets.client.render.entities.bosses;
 
-import java.util.List;
-
-import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelZombie;
-import net.minecraft.client.model.ModelZombieVillager;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
-import net.minecraft.client.renderer.entity.layers.LayerCustomHead;
-import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
-import net.minecraft.client.renderer.entity.layers.LayerRenderer;
-import net.minecraft.client.renderer.entity.layers.LayerVillagerArmor;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import com.google.common.collect.Lists;
 import com.mjr.extraplanets.entities.bosses.EntityEvolvedGiantZombieBoss;
 
 @SideOnly(Side.CLIENT)
 public class RenderEvolvedGiantZombieBoss extends RenderBiped<EntityEvolvedGiantZombieBoss> {
 	private static final ResourceLocation ZOMBIE_TEXTURES = new ResourceLocation("textures/entity/zombie/zombie.png");
-	private final ModelBiped defaultModel;
-	private final ModelZombieVillager zombieVillagerModel;
-	@SuppressWarnings("unused")
-	private final List<LayerRenderer<EntityEvolvedGiantZombieBoss>> villagerLayers;
-	private final List<LayerRenderer<EntityEvolvedGiantZombieBoss>> defaultLayers;
 
-	@SuppressWarnings("rawtypes")
 	public RenderEvolvedGiantZombieBoss(RenderManager renderManagerIn) {
-		super(renderManagerIn, new ModelZombie(), 0.5F, 1.0F);
-		LayerRenderer<?> layerrenderer = (LayerRenderer) this.layerRenderers.get(0);
-		this.defaultModel = this.modelBipedMain;
-		this.zombieVillagerModel = new ModelZombieVillager();
-		this.addLayer(new LayerHeldItem(this));
+		super(renderManagerIn, new ModelZombie(), 0.5F);
 		LayerBipedArmor layerbipedarmor = new LayerBipedArmor(this) {
 			protected void initArmor() {
 				this.modelLeggings = new ModelZombie(0.5F, true);
@@ -43,16 +24,6 @@ public class RenderEvolvedGiantZombieBoss extends RenderBiped<EntityEvolvedGiant
 			}
 		};
 		this.addLayer(layerbipedarmor);
-		this.defaultLayers = Lists.newArrayList(this.layerRenderers);
-
-		if (layerrenderer instanceof LayerCustomHead) {
-			this.removeLayer(layerrenderer);
-			this.addLayer(new LayerCustomHead(this.zombieVillagerModel.bipedHead));
-		}
-
-		this.removeLayer(layerbipedarmor);
-		this.addLayer(new LayerVillagerArmor(this));
-		this.villagerLayers = Lists.newArrayList(this.layerRenderers);
 	}
 
 	/**
@@ -68,7 +39,6 @@ public class RenderEvolvedGiantZombieBoss extends RenderBiped<EntityEvolvedGiant
 	 * Renders the desired {@code T} type Entity.
 	 */
 	public void doRender(EntityEvolvedGiantZombieBoss entity, double x, double y, double z, float entityYaw, float partialTicks) {
-		this.swapArmor(entity);
 		super.doRender(entity, x, y, z, entityYaw, partialTicks);
 	}
 
@@ -77,16 +47,5 @@ public class RenderEvolvedGiantZombieBoss extends RenderBiped<EntityEvolvedGiant
 	 */
 	protected ResourceLocation getEntityTexture(EntityEvolvedGiantZombieBoss entity) {
 		return ZOMBIE_TEXTURES;
-	}
-
-	private void swapArmor(EntityEvolvedGiantZombieBoss zombie) {
-		this.mainModel = this.defaultModel;
-		this.layerRenderers = this.defaultLayers;
-
-		this.modelBipedMain = (ModelBiped) this.mainModel;
-	}
-
-	protected void rotateCorpse(EntityEvolvedGiantZombieBoss entityLiving, float p_77043_2_, float p_77043_3_, float partialTicks) {
-		super.rotateCorpse(entityLiving, p_77043_2_, p_77043_3_, partialTicks);
 	}
 }
