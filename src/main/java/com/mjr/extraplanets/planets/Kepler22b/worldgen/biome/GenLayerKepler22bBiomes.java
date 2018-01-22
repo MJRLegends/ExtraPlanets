ad2557ed29a09799a4489e7c18fc540bd58bc2ab
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-22 23:48:48 +0000

    *Changed Kepler22b Rare biomes rate *Fixed Emerald Grit having the wrong name

--- src/main/java/com/mjr/extraplanets/planets/Kepler22b/worldgen/biome/GenLayerKepler22bBiomes.java
+++ src/main/java/com/mjr/extraplanets/planets/Kepler22b/worldgen/biome/GenLayerKepler22bBiomes.java
@@ -10,8 +10,8 @@ public class GenLayerKepler22bBiomes extends GenLayer {
 	protected BiomeGenBase[] commonBiomes = { Kepler22bBiomes.kepler22bPlains, Kepler22bBiomes.kepler22bBlueForest, Kepler22bBiomes.kepler22bPurpleForest, Kepler22bBiomes.kepler22bRedForest,
 			Kepler22bBiomes.kepler22bYellowForest, Kepler22bBiomes.kepler22bRedDesert, Kepler22bBiomes.kepler22bWasteLands, Kepler22bBiomes.kepler22bCandyLand };
 
-	protected Biome[] rareBiomes = { Kepler22bBiomes.kepler22bBlueForest, Kepler22bBiomes.kepler22bDiamondPlains, Kepler22bBiomes.kepler22bCoalPlains, Kepler22bBiomes.kepler22bIronPlains, Kepler22bBiomes.kepler22bGoldPlains,
-		Kepler22bBiomes.kepler22bEmeraldPlains };
+	protected Biome[] rareBiomes = { Kepler22bBiomes.kepler22bDiamondPlains, Kepler22bBiomes.kepler22bCoalPlains, Kepler22bBiomes.kepler22bIronPlains, Kepler22bBiomes.kepler22bGoldPlains,
+			Kepler22bBiomes.kepler22bEmeraldPlains };
 
 	public GenLayerKepler22bBiomes(long seed, GenLayer genlayer) {
 		super(seed);
@@ -30,7 +30,7 @@ public class GenLayerKepler22bBiomes extends GenLayer {
 			for (int dx = 0; dx < width; dx++) {
 				this.initChunkSeed(dx + x, dz + z);
 
-				if (this.nextInt(15) == 0) {
+				if (this.nextInt(150) == 0) {
 					dest[dz + dx * width] = rareBiomes[nextInt(rareBiomes.length)].biomeID;
 				} else {
 					dest[dz + dx * width] = commonBiomes[nextInt(commonBiomes.length)].biomeID;
