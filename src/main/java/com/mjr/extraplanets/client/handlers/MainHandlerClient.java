8371c03228b1dfb64583e609814e15e4482b69c3
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-22 19:43:06 +0000

    Fixed OpenGL Error spam

--- src/main/java/com/mjr/extraplanets/client/handlers/MainHandlerClient.java
+++ src/main/java/com/mjr/extraplanets/client/handlers/MainHandlerClient.java
@@ -280,8 +280,9 @@ public class MainHandlerClient {
 				event.density = 0.01f;
 				GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
 				event.setCanceled(true);
-			} else
-				GL11.glFogi(GL11.GL_EXP, GL11.GL_EXP);
+			} 
+			else
+				GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_LINEAR);
 		}
 	}
 
