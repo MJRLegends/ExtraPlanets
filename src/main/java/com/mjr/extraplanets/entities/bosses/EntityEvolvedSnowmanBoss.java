75a0d6b1ddf40ac4e4b5890c4573c1745affba1f
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-24 00:42:08 +0000

    Tweaks to the Snowman Boss moving speed

--- src/main/java/com/mjr/extraplanets/entities/bosses/EntityEvolvedSnowmanBoss.java
+++ src/main/java/com/mjr/extraplanets/entities/bosses/EntityEvolvedSnowmanBoss.java
@@ -70,7 +70,7 @@ public class EntityEvolvedSnowmanBoss extends EntityBossBase implements IRangedA
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(1250.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue((0.20000000298023224D * 8));
+		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.20000000298023224D);
 	}
 
     @Nullable
