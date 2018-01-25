cfd8a821aadf1991a729187d6091c75eae20d03a
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-25 08:19:41 +0000

    Changed Galacticraft minimum requirement to build 122

--- src/main/java/com/mjr/extraplanets/handlers/GalacticraftVersionChecker.java
+++ src/main/java/com/mjr/extraplanets/handlers/GalacticraftVersionChecker.java
@@ -1,18 +1,30 @@
 package com.mjr.extraplanets.handlers;
 
+import java.lang.reflect.Method;
+
+import micdoodle8.mods.galacticraft.core.dimension.WorldProviderMoon;
+
 import com.mjr.extraplanets.Constants;
 import com.mjr.mjrlegendslib.util.MessageUtilities;
 
 public class GalacticraftVersionChecker {
 	@SuppressWarnings("unused")
 	public static void run() {
-		if (micdoodle8.mods.galacticraft.core.Constants.LOCALBUILDVERSION >= 119) {
-			MessageUtilities.infoMessageToLog(Constants.modID, "Correct Galacticraft Version Found!, " + Constants.modName + " has detected you are using the required minimum version or higher of Galacticraft!");
-		} else {
-			MessageUtilities.fatalErrorMessageToLog(Constants.modID, "has detected you are using a version below the required minimum version of build #119, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
+		try {
+			Method m = WorldProviderMoon.class.getMethod("getSurfaceBlocks");
+		} catch (NoSuchMethodException e) {
+			MessageUtilities.fatalErrorMessageToLog(Constants.modID, "has detected you are using a version below the required minimum version of build #122, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
 			MessageUtilities.errorMessageBox(Constants.modID, "Incompatable error",
-					"has detected you are using a version below the required minimum version of build #119 </font><font Color=blue> <br> You can find it here https://micdoodle8.com/mods/galacticraft/downloads", 600, 600);
-			MessageUtilities.throwCrashError(Constants.modName + ": has detected you are using a version below the required minimum version of build #119, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
+					"has detected you are using a version below the required minimum version of build #122 </font><font Color=blue> <br> You can find it here https://micdoodle8.com/mods/galacticraft/downloads", 600, 600);
+			MessageUtilities.throwCrashError(Constants.modName + ": has detected you are using a version below the required minimum version of build #122, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
 		}
+//		if (micdoodle8.mods.galacticraft.core.Constants.LOCALBUILDVERSION >= 119) {
+//			MessageUtilities.infoMessageToLog(Constants.modID, "Correct Galacticraft Version Found!, " + Constants.modName + " has detected you are using the required minimum version or higher of Galacticraft!");
+//		} else {
+//			MessageUtilities.fatalErrorMessageToLog(Constants.modID, "has detected you are using a version below the required minimum version of build #119, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
+//			MessageUtilities.errorMessageBox(Constants.modID, "Incompatable error",
+//					"has detected you are using a version below the required minimum version of build #119 </font><font Color=blue> <br> You can find it here https://micdoodle8.com/mods/galacticraft/downloads", 600, 600);
+//			MessageUtilities.throwCrashError(Constants.modName + ": has detected you are using a version below the required minimum version of build #119, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
+//		}
 	}
 }
