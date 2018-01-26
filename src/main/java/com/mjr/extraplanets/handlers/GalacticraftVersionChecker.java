7beb767d3fea65cd0f92a35acbee5335490c073d
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-26 17:26:49 +0000

    Fixed Galacticraft Version Checker for dev workspace

--- src/main/java/com/mjr/extraplanets/handlers/GalacticraftVersionChecker.java
+++ src/main/java/com/mjr/extraplanets/handlers/GalacticraftVersionChecker.java
@@ -6,7 +6,7 @@ import com.mjr.mjrlegendslib.util.MessageUtilities;
 public class GalacticraftVersionChecker {
 	@SuppressWarnings("unused")
 	public static void run() {
-		if (micdoodle8.mods.galacticraft.core.Constants.LOCALBUILDVERSION >= 119) {
+		if (micdoodle8.mods.galacticraft.core.Constants.LOCALBUILDVERSION >= 122 || micdoodle8.mods.galacticraft.core.Constants.LOCALBUILDVERSION >= -1) {
 			MessageUtilities.infoMessageToLog(Constants.modID, "Correct Galacticraft Version Found!, " + Constants.modName + " has detected you are using the required minimum version or higher of Galacticraft!");
 		} else {
 			MessageUtilities.fatalErrorMessageToLog(Constants.modID, "has detected you are using a version below the required minimum version of build #119, You can find it here https://micdoodle8.com/mods/galacticraft/downloads");
