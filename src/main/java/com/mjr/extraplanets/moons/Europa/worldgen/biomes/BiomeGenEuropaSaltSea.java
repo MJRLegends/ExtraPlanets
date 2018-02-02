eaae11b9433581156927332837f210f74fe24277
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-02 21:06:29 +0000

    Fixed crash on load up for biomes due to recent commit

--- src/main/java/com/mjr/extraplanets/moons/Europa/worldgen/biomes/BiomeGenEuropaSaltSea.java
+++ src/main/java/com/mjr/extraplanets/moons/Europa/worldgen/biomes/BiomeGenEuropaSaltSea.java
@@ -9,7 +9,7 @@ import net.minecraftforge.common.BiomeDictionary;
 import com.google.common.collect.Lists;
 import com.mjr.extraplanets.Config;
 import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
-import com.mjr.extraplanets.blocks.planetAndMoonBlocks.BlockBasicJupiter;
+import com.mjr.extraplanets.blocks.planetAndMoonBlocks.BlockBasicEuropa;
 import com.mjr.extraplanets.moons.Europa.worldgen.EuropaBiomes;
 
 public class BiomeGenEuropaSaltSea extends EuropaBiomes {
@@ -19,7 +19,7 @@ public class BiomeGenEuropaSaltSea extends EuropaBiomes {
 			BiomeDictionary.registerBiomeType(this, BiomeDictionary.Type.COLD, BiomeDictionary.Type.WET, BiomeDictionary.Type.DEAD, BiomeDictionary.Type.SNOWY, BiomeDictionary.Type.OCEAN);
 		this.spawnableCreatureList.clear();
 		this.topBlock = ExtraPlanets_Blocks.EUROPA_BLOCKS.getDefaultState();
-		this.fillerBlock = ExtraPlanets_Blocks.EUROPA_BLOCKS.getDefaultState().withProperty(BlockBasicJupiter.BASIC_TYPE, BlockBasicJupiter.EnumBlockBasic.SUB_SURFACE);
+		this.fillerBlock = ExtraPlanets_Blocks.EUROPA_BLOCKS.getDefaultState().withProperty(BlockBasicEuropa.BASIC_TYPE, BlockBasicEuropa.EnumBlockBasic.SUB_SURFACE);
 	}
 
 	@Override
--- src/main/java/com/mjr/extraplanets/moons/Titan/worldgen/biomes/BiomeGenTitanMethaneSea.java
+++ src/main/java/com/mjr/extraplanets/moons/Titan/worldgen/biomes/BiomeGenTitanMethaneSea.java
@@ -9,7 +9,7 @@ import net.minecraftforge.common.BiomeDictionary;
 import com.google.common.collect.Lists;
 import com.mjr.extraplanets.Config;
 import com.mjr.extraplanets.blocks.ExtraPlanets_Blocks;
-import com.mjr.extraplanets.blocks.planetAndMoonBlocks.BlockBasicJupiter;
+import com.mjr.extraplanets.blocks.planetAndMoonBlocks.BlockBasicTitan;
 import com.mjr.extraplanets.moons.Titan.worldgen.TitanBiomes;
 
 public class BiomeGenTitanMethaneSea extends TitanBiomes {
@@ -18,7 +18,7 @@ public class BiomeGenTitanMethaneSea extends TitanBiomes {
 		if (Config.REGISTER_BIOME_TYPES)
 			BiomeDictionary.registerBiomeType(this, BiomeDictionary.Type.COLD, BiomeDictionary.Type.WET, BiomeDictionary.Type.DEAD, BiomeDictionary.Type.OCEAN);
 		this.topBlock = ExtraPlanets_Blocks.TITAN_BLOCKS.getDefaultState();
-		this.fillerBlock = ExtraPlanets_Blocks.TITAN_BLOCKS.getDefaultState().withProperty(BlockBasicJupiter.BASIC_TYPE, BlockBasicJupiter.EnumBlockBasic.SUB_SURFACE);
+		this.fillerBlock = ExtraPlanets_Blocks.TITAN_BLOCKS.getDefaultState().withProperty(BlockBasicTitan.BASIC_TYPE, BlockBasicTitan.EnumBlockBasic.SUB_SURFACE);
 	}
 
 	@Override
