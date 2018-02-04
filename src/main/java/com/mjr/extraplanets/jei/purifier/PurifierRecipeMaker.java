ed16c8ac56c8894a899542bbd0b8bbc2a0e265f2
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-04 17:38:41 +0000

    Few more fixes for Basic Purifier JEI Support

--- src/main/java/com/mjr/extraplanets/jei/purifier/PurifierRecipeMaker.java
+++ src/main/java/com/mjr/extraplanets/jei/purifier/PurifierRecipeMaker.java
@@ -15,20 +15,20 @@ public class PurifierRecipeMaker {
 		List<PurifierRecipeWrapper> recipes = new ArrayList<>();
 		List<ItemStack> inputs = new ArrayList<>();
 		inputs.add(FluidUtil.getFilledBucket(new FluidStack(ExtraPlanets_Fluids.RADIO_ACTIVE_WATER_FLUID, 1000)));
-		inputs.add(null);
+		inputs.add(ItemStack.EMPTY);
 		inputs.add(new ItemStack(ExtraPlanets_Items.IODIDE_SALT, 6, 0));
 		List<ItemStack> outputs = new ArrayList<>();
 		outputs.add(FluidUtil.getFilledBucket(new FluidStack(ExtraPlanets_Fluids.INFECTED_WATER_FLUID, 1000)));
-		outputs.add(null);
+		outputs.add(ItemStack.EMPTY);
 		PurifierRecipeWrapper wrapper = new PurifierRecipeWrapper(inputs, outputs);
 		recipes.add(wrapper);
 
 		List<ItemStack> inputs2 = new ArrayList<>();
-		inputs2.add(null);
+		inputs2.add(ItemStack.EMPTY);
 		inputs2.add(FluidUtil.getFilledBucket(new FluidStack(ExtraPlanets_Fluids.INFECTED_WATER_FLUID, 1000)));
 		inputs2.add(new ItemStack(ExtraPlanets_Items.IODIDE_SALT, 6, 0));
 		List<ItemStack> outputs2 = new ArrayList<>();
-		outputs2.add(null);
+		outputs2.add(ItemStack.EMPTY);
 		outputs2.add(FluidUtil.getFilledBucket(new FluidStack(ExtraPlanets_Fluids.CLEAN_WATER_FLUID, 1000)));
 		PurifierRecipeWrapper wrapper2 = new PurifierRecipeWrapper(inputs2, outputs2);
 		recipes.add(wrapper2);
