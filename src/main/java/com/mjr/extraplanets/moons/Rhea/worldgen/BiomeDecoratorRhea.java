ecd2adc2481efd61de7cb5d278e8db36a41b3154
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-11 01:51:49 +0000

    Fixed missing gravel generation on Rhea

--- src/main/java/com/mjr/extraplanets/moons/Rhea/worldgen/BiomeDecoratorRhea.java
+++ src/main/java/com/mjr/extraplanets/moons/Rhea/worldgen/BiomeDecoratorRhea.java
@@ -13,7 +13,7 @@ public class BiomeDecoratorRhea extends BiomeDecoratorSpace {
 	private WorldGenerator copperGen;
 	private WorldGenerator tinGen;
 	private WorldGenerator ironGen;
-	// private WorldGenerator gravelGen;
+	private WorldGenerator gravelGen;
 	private WorldGenerator fossilsGen;
 
 	private World currentWorld;
@@ -24,7 +24,7 @@ public class BiomeDecoratorRhea extends BiomeDecoratorSpace {
 			this.tinGen = new WorldGenMinableMeta(ExtraPlanets_Blocks.RHEA_BLOCKS, 4, 4, true, ExtraPlanets_Blocks.RHEA_BLOCKS, 2);
 			this.ironGen = new WorldGenMinableMeta(ExtraPlanets_Blocks.RHEA_BLOCKS, 8, 3, true, ExtraPlanets_Blocks.RHEA_BLOCKS, 2);
 		}
-		// this.gravelGen = new WorldGenMinableMeta(ExtraPlanetsBlocks.rheaGravel, 12, 0, true, ExtraPlanetsBlocks.RHEA_BLOCKS, 2);
+		this.gravelGen = new WorldGenMinableMeta(ExtraPlanets_Blocks.RHEA_GRAVEL, 12, 0, true, ExtraPlanets_Blocks.RHEA_BLOCKS, 2);
 		this.fossilsGen = new WorldGenMinableMeta(ExtraPlanets_Blocks.FOSSIL, 3, 0, true, ExtraPlanets_Blocks.RHEA_BLOCKS, 1);
 
 		// WorldGenMinableMeta(Block OreBlock, int numberOfBlocks, int OreMeta, boolean usingMetaData, Block StoneBlock, int StoneMeta);
@@ -47,7 +47,7 @@ public class BiomeDecoratorRhea extends BiomeDecoratorSpace {
 			this.generateOre(23, this.tinGen, 0, 60);
 			this.generateOre(20, this.ironGen, 0, 64);
 		}
-		// this.generateOre(15, this.gravelGen, 0, 80);
+		this.generateOre(15, this.gravelGen, 0, 80);
 		this.generateOre(10, this.fossilsGen, 0, 256);
 
 		// generateOre(int amountPerChunk, WorldGenerator worldGenerator, int minY, int maxY);
