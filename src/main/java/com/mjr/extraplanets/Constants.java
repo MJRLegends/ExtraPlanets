5ad3f3e2489a5e4ac1afa72cc87ecdf7cedb3230
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-11 22:16:00 +0000

    Missing from last commit

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 
 	public static final String MCVERSION = "[1.11.2]";
 	public static final String DEPENDENCIES_FORGE = "required-after:forge@[13.20.1.2513,);";
-	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.11.2-1.0.4,); required-after:galacticraftcore; required-after:galacticraftplanets;";
+	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.11.2-1.0.4,); required-after:galacticraftcore@[4.0.1.133,); required-after:galacticraftplanets@[4.0.1.133,);";
 
 	public static final String GUIFACTORY = "com.mjr.extraplanets.client.gui.screen.ConfigGuiFactory";
 	public static final String CERTIFICATEFINGERPRINT = "b02331787272ec3515ebe63ecdeea0d746653468";
