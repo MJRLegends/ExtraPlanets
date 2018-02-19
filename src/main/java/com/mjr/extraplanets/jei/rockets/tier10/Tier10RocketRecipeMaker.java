8ccdafa1045d1e9d66108731b75ccf804d4c5fe4
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-19 02:30:56 +0000

    Changes to Rockets/Vehicles JEI Support to match GC changes

--- src/main/java/com/mjr/extraplanets/jei/rockets/tier10/Tier10RocketRecipeMaker.java
+++ src/main/java/com/mjr/extraplanets/jei/rockets/tier10/Tier10RocketRecipeMaker.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import micdoodle8.mods.galacticraft.api.recipe.INasaWorkbenchRecipe;
+import micdoodle8.mods.galacticraft.core.client.jei.tier1rocket.Tier1RocketRecipeMaker;
 
 import com.mjr.extraplanets.recipes.Tier10RocketRecipes;
 
@@ -11,7 +12,12 @@ public class Tier10RocketRecipeMaker {
 	public static List<Tier10RocketRecipeWrapper> getRecipesList() {
 		List<Tier10RocketRecipeWrapper> recipes = new ArrayList<>();
 
+		int chestCount = -1;
 		for (INasaWorkbenchRecipe recipe : Tier10RocketRecipes.getTier10RocketRecipes()) {
+            int chests = Tier1RocketRecipeMaker.countChests(recipe); 
+            if (chests == chestCount)
+                continue;
+            chestCount = chests;
 			Tier10RocketRecipeWrapper wrapper = new Tier10RocketRecipeWrapper(recipe);
 			recipes.add(wrapper);
 		}
