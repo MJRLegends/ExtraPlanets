95c6c2ce8b7d42b2199a9557e373b3f66378036d
MJRLegends <MJRLegends@users.noreply.github.com>
2018-03-06 01:34:24 +0000

    Released v1.0.0

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -4,9 +4,9 @@ public class Constants {
 	public static final String modID = "extraplanets";
 	public static final String modName = "ExtraPlanets";
 
-	public static final int LOCALMAJVERSION = 0;
-	public static final int LOCALMINVERSION = 9;
-	public static final int LOCALBUILDVERSION = 9;
+	public static final int LOCALMAJVERSION = 1;
+	public static final int LOCALMINVERSION = 0;
+	public static final int LOCALBUILDVERSION = 0;
 	public static final String modVersion = "1.10.2" + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
 
 	public static final String MCVERSION = "[1.10.2]";
