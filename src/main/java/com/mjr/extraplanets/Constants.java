e58be2fef2bcd0bb8cd054a4f9c2036efe3e385c
MJRLegends <MJRLegends@users.noreply.github.com>
2018-03-12 20:36:51 +0000

    Released v0.6.6

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -6,12 +6,12 @@ public class Constants {
 
 	public static final int LOCALMAJVERSION = 0;
 	public static final int LOCALMINVERSION = 6;
-	public static final int LOCALBUILDVERSION = 5;
+	public static final int LOCALBUILDVERSION = 6;
 	public static final String modVersion = "1.11.2" + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
 
 	public static final String MCVERSION = "[1.11.2]";
 	public static final String DEPENDENCIES_FORGE = "required-after:forge@[13.20.1.2513,);";
-	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.11.2-1.0.8,); required-after:galacticraftcore@[4.0.1.144,); required-after:galacticraftplanets@[4.0.1.144,);";
+	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.11.2-1.0.8,); required-after:galacticraftcore@[4.0.1.152,); required-after:galacticraftplanets@[4.0.1.152,);";
 
 	public static final String GUIFACTORY = "com.mjr.extraplanets.client.gui.screen.ConfigGuiFactory";
 	public static final String CERTIFICATEFINGERPRINT = "b02331787272ec3515ebe63ecdeea0d746653468";
