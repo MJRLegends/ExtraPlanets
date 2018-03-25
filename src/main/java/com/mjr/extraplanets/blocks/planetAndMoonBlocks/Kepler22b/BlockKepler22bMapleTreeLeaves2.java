4a07f9c7fb40ef3b612be6042125851fa3e79065
MJRLegends <MJRLegends@users.noreply.github.com>
2018-03-25 03:21:07 +0100

    Fixed issue with Kepler22b Brown & Green Leaves when picking the block in creative mode

--- src/main/java/com/mjr/extraplanets/blocks/planetAndMoonBlocks/Kepler22b/BlockKepler22bMapleTreeLeaves2.java
+++ src/main/java/com/mjr/extraplanets/blocks/planetAndMoonBlocks/Kepler22b/BlockKepler22bMapleTreeLeaves2.java
@@ -124,7 +124,7 @@ public class BlockKepler22bMapleTreeLeaves2 extends BlockLeaves {
 
 	@Override
 	public int damageDropped(IBlockState state) {
-		return state.getValue(VARIANT).getMetadata() + 4;
+		return state.getValue(VARIANT).getMetadata();
 	}
 
 	@Override
