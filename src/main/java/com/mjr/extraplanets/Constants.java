b23bf9becab199a1afcc58c91a78cb667bbb43c1
MJRLegends <MJRLegends@users.noreply.github.com>
2018-04-05 01:09:27 +0100

    Released v2.0.1

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
 	public static final int LOCALMAJVERSION = 2;
 	public static final int LOCALMINVERSION = 0;
-	public static final int LOCALBUILDVERSION = 0;
+	public static final int LOCALBUILDVERSION = 1;
 
 	public static final String modVersion = "1.7.10" + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
 
