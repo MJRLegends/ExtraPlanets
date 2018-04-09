9c8ed125e3a78d753b7f638706b87595483520c4
MJRLegends <MJRLegends@users.noreply.github.com>
2018-04-09 19:07:43 +0100

    Added Tier 1 - 4 Space Suit chest with Jet Pack (WIP)

---
+++ src/main/java/com/mjr/extraplanets/items/armor/bases/JetpackArmorBase.java
@@ -0,0 +1,33 @@
+package com.mjr.extraplanets.items.armor.bases;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import com.mjr.extraplanets.api.IJetpackArmour;
+
+public abstract class JetpackArmorBase extends ElectricArmorBase implements IJetpackArmour {
+	public boolean activeJetPack = false;
+
+	public JetpackArmorBase(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(materialIn, renderIndexIn, equipmentSlotIn);
+	}
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
+		if (activeJetPack){
+			useJetPack(player);
+			this.discharge(stack, this.powerPerTick(), true);
+		}
+	}
+
+	public void useJetPack(EntityPlayer player) {
+		player.motionY = Math.min(player.motionY + 0.5, this.getJetpackAccelSpeed());
+		player.fallDistance = 0.0F;
+	}
+
+	@Override
+	public abstract double getJetpackAccelSpeed();
+	public abstract float powerPerTick();
+}
