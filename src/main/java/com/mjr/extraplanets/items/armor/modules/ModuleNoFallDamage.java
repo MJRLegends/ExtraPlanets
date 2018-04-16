c4d339d040e49ea7edba37e796fc5b7d5dc25714
MJRLegends <MJRLegends@users.noreply.github.com>
2018-04-16 01:08:58 +0100

    Added No Fall Damage Module & Item

---
+++ src/main/java/com/mjr/extraplanets/items/armor/modules/ModuleNoFallDamage.java
@@ -0,0 +1,38 @@
+package com.mjr.extraplanets.items.armor.modules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+
+import com.mjr.extraplanets.items.ExtraPlanets_Items;
+
+public class ModuleNoFallDamage extends Module {
+
+	public ModuleNoFallDamage(String name) {
+		super(name, 0, new ItemStack(ExtraPlanets_Items.MODULE_ITEMS), true);
+		List<ItemStack> items = new ArrayList<ItemStack>();
+		items.add(new ItemStack(ExtraPlanets_Items.MODULE_ITEMS, 1, 0));
+		this.setRequirements(items);
+	}
+
+	@Override
+	public void tickServer(EntityPlayerMP player) {
+		if (player.fallDistance != 0F)
+			player.fallDistance = 0F;
+	}
+
+	@Override
+	public void tickClient(EntityPlayerSP player) {
+
+	}
+
+	@Override
+	public void renderHelmetOverlay(ItemStack stack, EntityPlayer player, ScaledResolution resolution, float partialTicks) {
+
+	}
+}
