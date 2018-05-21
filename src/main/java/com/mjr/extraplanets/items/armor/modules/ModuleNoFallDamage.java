445351fb122c1c97ebc31c897d73e90903c36ae8
MJRLegends <MJRLegends@users.noreply.github.com>
2018-05-21 15:12:03 +0100

    Fixed for MC 1.10 & below

--- src/main/java/com/mjr/extraplanets/items/armor/modules/ModuleNoFallDamage.java
+++ src/main/java/com/mjr/extraplanets/items/armor/modules/ModuleNoFallDamage.java
@@ -23,7 +23,7 @@ public class ModuleNoFallDamage extends Module {
 	@Override
 	public void tickServer(EntityPlayerMP player) {
 		if (player.fallDistance != 0F) {
-			if ((player.ticksExisted - 1) % 20 == 0)ModuleHelper.takeArmourPower(player.inventory.armorInventory.get(this.getSlotType()), this.getUsePowerCost());
+			if ((player.ticksExisted - 1) % 20 == 0)ModuleHelper.takeArmourPower(player.inventory.armorInventory[this.getSlotType()], this.getUsePowerCost());
 			player.fallDistance = 0F;
 		}
 	}
