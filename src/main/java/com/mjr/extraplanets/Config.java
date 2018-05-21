0e7820cd249ba5e86239cc3e65577f4123d49a9d
MJRLegends <MJRLegends@users.noreply.github.com>
2018-05-22 00:46:49 +0100

    Added a Satellite Tower Structure to Ceres

--- src/main/java/com/mjr/extraplanets/Config.java
+++ src/main/java/com/mjr/extraplanets/Config.java
@@ -37,6 +37,7 @@ public class Config {
 	public static boolean GENERATE_URANUS_ICE_SPIKES;
 	public static boolean GENERATE_JUITPER_SKY_FEATURE;
 	public static boolean GENERATE_MERCURY_METEORS;
+	public static boolean GENERATE_CERES_SATELLITE_TOWER;
 	public static boolean JUITPER_LIGHTING;
 	public static boolean CUSTOM_FOG;
 
@@ -447,6 +448,7 @@ public class Config {
 		GENERATE_URANUS_ICE_SPIKES = config.get(Constants.CONFIG_CATEGORY_DIMENSION_SETTINGS, "Generate Ice Spikes on Uranus", true, "Setting this option to false will disable & remove Ice Spikes from generating on Uranus").getBoolean(true);
 		GENERATE_JUITPER_SKY_FEATURE = config.get(Constants.CONFIG_CATEGORY_DIMENSION_SETTINGS, "Generate Sky Feature on Jupiter", true, "Setting this option to false will disable & remove the Sky Feature on Jupiter").getBoolean(true);
 		GENERATE_MERCURY_METEORS = config.get(Constants.CONFIG_CATEGORY_DIMENSION_SETTINGS, "Generate Meteors on Mercury", true, "Setting this option to false will disable & remove the Meteors on Mercury").getBoolean(true);
+		GENERATE_CERES_SATELLITE_TOWER = config.get(Constants.CONFIG_CATEGORY_DIMENSION_SETTINGS, "Generate Satellite Towers on Ceres", true, "Setting this option to false will disable & remove the Satellite Towers on Ceres").getBoolean(true);
 		JUITPER_LIGHTING = config.get(Constants.CONFIG_CATEGORY_DIMENSION_SETTINGS, "Enable Lighting & Lighting Effects on Jupiter", true, "Setting this option to false will disable & remove Lighting & Lighting Effects on Jupiter").getBoolean(true);
 		CUSTOM_FOG = config.get(Constants.CONFIG_CATEGORY_DIMENSION_SETTINGS, "Enable Custom Fog Effect on Jupiter, Uranus, Saturn, Neptune", true, "Setting this option to false will disable & remove Fog Effects on Jupiter, Uranus, Saturn, Neptune")
 				.getBoolean(true);
