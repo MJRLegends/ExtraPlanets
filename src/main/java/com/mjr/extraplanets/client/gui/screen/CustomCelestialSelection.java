5a396ddc6faeb6e154bd4537e22b24e89e84fde8
MJRLegends <MJRLegends@users.noreply.github.com>
2018-05-21 16:15:19 +0100

    Fixes for MC 1.11 & below

--- src/main/java/com/mjr/extraplanets/client/gui/screen/CustomCelestialSelection.java
+++ src/main/java/com/mjr/extraplanets/client/gui/screen/CustomCelestialSelection.java
@@ -35,8 +35,8 @@ import micdoodle8.mods.galacticraft.planets.mars.dimension.WorldProviderMars;
 import micdoodle8.mods.galacticraft.planets.venus.dimension.WorldProviderVenus;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.WorldProvider;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.FMLClientHandler;
@@ -169,7 +169,7 @@ public class CustomCelestialSelection extends GuiCelestialSelection {
 			if (alpha > 0.0F) {
 				GlStateManager.pushMatrix();
 				Matrix4f worldMatrixLocal = setupMatrix(body, worldMatrix, fb, hasParent ? 0.25F : 1.0F);
-				this.mc.renderEngine.bindTexture(TextureManager.RESOURCE_LOCATION_EMPTY);
+				this.mc.renderEngine.bindTexture(new ResourceLocation(""));
 				if (!this.isZoomed() && !(body instanceof Moon) && !(body instanceof Satellite) && !(body instanceof Star)) {
 					this.drawCenteredString(this.fontRendererObj, body.getLocalizedName(), 0, 5, 14737632);
 				} else if (this.isZoomed() && (body instanceof Moon) && !(body instanceof Satellite) && !(body instanceof Star)) {
