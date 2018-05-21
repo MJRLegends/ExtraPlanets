c50c0eddd42c2247fdc5bfe592493cf7d37d9a76
MJRLegends <MJRLegends@users.noreply.github.com>
2018-05-21 15:59:55 +0100

    Fixed crash issue with having certain parts of the space suit on at a time

--- src/main/java/com/mjr/extraplanets/items/armor/Tier4SpaceSuitArmor.java
+++ src/main/java/com/mjr/extraplanets/items/armor/Tier4SpaceSuitArmor.java
@@ -140,25 +140,25 @@ public class Tier4SpaceSuitArmor extends ElectricArmorBase implements IPressureS
 		ItemStack leggins = player.inventory.armorInventory[1];
 		ItemStack boots = player.inventory.armorInventory[0];
 
-		if (helmet.getItem() instanceof IModularArmor)
+		if (helmet != null && helmet.getItem() instanceof IModularArmor)
 			for (Module hemletModules : ModuleHelper.getModules(helmet)) {
 				if (hemletModules.isActive())
 					if (ModuleHelper.hasPower(helmet, ModuleHelper.getModuleUseCost(hemletModules)))
 						hemletModules.renderHelmetOverlay(stack, player, resolution, partialTicks);
 			}
-		if (chest.getItem() instanceof IModularArmor)
+		if (chest != null && chest.getItem() instanceof IModularArmor)
 			for (Module chestModules : ModuleHelper.getModules(chest)) {
 				if (chestModules.isActive())
 					if (ModuleHelper.hasPower(helmet, ModuleHelper.getModuleUseCost(chestModules)))
 						chestModules.renderHelmetOverlay(stack, player, resolution, partialTicks);
 			}
-		if (leggins.getItem() instanceof IModularArmor)
+		if (leggins != null && leggins.getItem() instanceof IModularArmor)
 			for (Module legginsModules : ModuleHelper.getModules(leggins)) {
 				if (legginsModules.isActive())
 					if (ModuleHelper.hasPower(helmet, ModuleHelper.getModuleUseCost(legginsModules)))
 						legginsModules.renderHelmetOverlay(stack, player, resolution, partialTicks);
 			}
-		if (boots.getItem() instanceof IModularArmor)
+		if (boots != null && boots.getItem() instanceof IModularArmor)
 			for (Module bootsModules : ModuleHelper.getModules(boots)) {
 				if (bootsModules.isActive())
 					if (ModuleHelper.hasPower(helmet, ModuleHelper.getModuleUseCost(bootsModules)))
