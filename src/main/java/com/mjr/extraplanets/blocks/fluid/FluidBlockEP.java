bc10e59edfc912b40adf5abf2383f7987e1058a8
MJRLegends <MJRLegends@users.noreply.github.com>
2019-03-01 23:30:29 +0000

    Fixed issue #405 (Commented out pointless code that needs fixing for real seems to be left over from porting from 1.7.10)

--- src/main/java/com/mjr/extraplanets/blocks/fluid/FluidBlockEP.java
+++ src/main/java/com/mjr/extraplanets/blocks/fluid/FluidBlockEP.java
@@ -52,18 +52,18 @@ public class FluidBlockEP extends FluidBasicBlock {
 		}
 	}
 
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void randomDisplayTick(World world, BlockPos pos, IBlockState state, Random rand) {
-		super.randomDisplayTick(world, pos, state, rand);
-
-		if (rand.nextInt(1200) == 0) {
-			world.playSound(pos.getX() + 0.5F, pos.getY() + 0.5F, pos.getZ() + 0.5F, "liquid.lava", rand.nextFloat() * 0.25F + 0.75F, 0.00001F + rand.nextFloat() * 0.5F, false);
-		}
-		if (rand.nextInt(10) == 0) {
-			if (World.doesBlockHaveSolidTopSurface(world, pos.down()) && !world.getBlockState(pos.down(2)).getBlock().getMaterial().blocksMovement()) {
-				GalacticraftCore.proxy.spawnParticle("", new Vector3(pos.getX() + rand.nextFloat(), pos.getY() - 1.05D, pos.getZ() + rand.nextFloat()), new Vector3(0, 0, 0), new Object[] {});
-			}
-		}
-	}
+//	@Override TODO Fix
+//	@SideOnly(Side.CLIENT)
+//	public void randomDisplayTick(World world, BlockPos pos, IBlockState state, Random rand) {
+//		super.randomDisplayTick(world, pos, state, rand);
+//
+//		if (rand.nextInt(1200) == 0) {
+//			world.playSound(pos.getX() + 0.5F, pos.getY() + 0.5F, pos.getZ() + 0.5F, "liquid.lava", rand.nextFloat() * 0.25F + 0.75F, 0.00001F + rand.nextFloat() * 0.5F, false);
+//		}
+//		if (rand.nextInt(10) == 0) {
+//			if (World.doesBlockHaveSolidTopSurface(world, pos.down()) && !world.getBlockState(pos.down(2)).getBlock().getMaterial().blocksMovement()) {
+//				GalacticraftCore.proxy.spawnParticle("", new Vector3(pos.getX() + rand.nextFloat(), pos.getY() - 1.05D, pos.getZ() + rand.nextFloat()), new Vector3(0, 0, 0), new Object[] {});
+//			}
+//		}
+//	}
 }
