8d064859dc3ae6974a7d351697eb0f4150fb5a1c
MJRLegends <MJRLegends@users.noreply.github.com>
2019-03-26 21:32:02 +0000

    Fixes for Basic Densifier/Purifier machines

--- src/main/java/com/mjr/extraplanets/tileEntities/machines/TileEntityBasicCrystallizer.java
+++ src/main/java/com/mjr/extraplanets/tileEntities/machines/TileEntityBasicCrystallizer.java
@@ -83,8 +83,8 @@ public class TileEntityBasicCrystallizer extends TileBaseElectricBlockWithInvent
 	}
 
 	private void checkFluidTankTransfer(int slot, FluidTank tank) {
-		if (this.getStackInSlot(slot) != null) {
-			ItemStack item = this.getStackInSlot(slot);
+		ItemStack item = this.getStackInSlot(slot);
+		if (item != null && FluidUtil.isValidContainer(item)) {
 			FluidStack stack = FluidUtil.getFluidContained(item);
 			if (FluidUtil.isEmptyContainer(this.getStackInSlot(slot)) == false && stack != null && stack.getFluid() != null && stack.getFluid().equals(ExtraPlanets_Fluids.SALT_FLUID)) {
 				FluidUtil.loadFromContainer(inputTank, ExtraPlanets_Fluids.SALT_FLUID, stacks, slot, stack.amount);
