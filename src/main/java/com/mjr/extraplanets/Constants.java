e6bfd3e8bcbd9b8eb6fbea6019ccc4c1226e40b2
MJRLegends <MJRLegends@users.noreply.github.com>
2019-07-23 00:41:27 +0100

    Set minimum version of MJRLegendsLib requirement to v1.1.5

--- src/main/java/com/mjr/extraplanets/Constants.java
+++ src/main/java/com/mjr/extraplanets/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 
 	public static final String MCVERSION = "[1.11.2]";
 	public static final String DEPENDENCIES_FORGE = "required-after:forge@[13.20.1.2513,);";
-	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.11.2-1.1.3,); required-after:galacticraftcore@[4.0.1.178,); required-after:galacticraftplanets@[4.0.1.178,);";
+	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.11.2-1.1.5,); required-after:galacticraftcore@[4.0.1.178,); required-after:galacticraftplanets@[4.0.1.178,);";
 
 	public static final String GUIFACTORY = "com.mjr.extraplanets.client.gui.screen.ConfigGuiFactory";
 	public static final String CERTIFICATEFINGERPRINT = "b02331787272ec3515ebe63ecdeea0d746653468";
