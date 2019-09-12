3ef527100b408fdf493fc058c5e160fc22d32fbf
MJRLegends <MJRLegends@users.noreply.github.com>
2019-09-12 01:44:00 +0100

    Fixed issue #475

--- src/main/java/com/mjr/extraplanets/items/armor/Tier2SpaceSuitArmor.java
+++ src/main/java/com/mjr/extraplanets/items/armor/Tier2SpaceSuitArmor.java
@@ -67,7 +67,7 @@ public class Tier2SpaceSuitArmor extends ElectricArmorBase implements IPressureS
 	public int gravityOverrideIfLow(EntityPlayer p) {
 		for (int i = 0; i < 4; i++)
 			if (p.inventory.armorInventory.get(i) != null)
-				if (p.inventory.armorInventory.get(i).getItem() == ExtraPlanets_Armor.TIER_2_SPACE_SUIT_BOOTS)
+				if (p.inventory.armorInventory.get(i).getItem() == ExtraPlanets_Armor.TIER_2_SPACE_SUIT_GRAVITY_BOOTS)
 					return 55;
 		return 0;
 	}
@@ -76,7 +76,7 @@ public class Tier2SpaceSuitArmor extends ElectricArmorBase implements IPressureS
 	public int gravityOverrideIfHigh(EntityPlayer p) {
 		for (int i = 0; i < 4; i++)
 			if (p.inventory.armorInventory.get(i) != null)
-				if (p.inventory.armorInventory.get(i).getItem() == ExtraPlanets_Armor.TIER_2_SPACE_SUIT_BOOTS)
+				if (p.inventory.armorInventory.get(i).getItem() == ExtraPlanets_Armor.TIER_2_SPACE_SUIT_GRAVITY_BOOTS)
 					return 75;
 		return 0;
 	}
