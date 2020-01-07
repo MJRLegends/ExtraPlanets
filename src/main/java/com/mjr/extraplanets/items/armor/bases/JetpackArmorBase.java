09b0b3e282acfb4574ce6bc38e86a98ef0bc5f2d
MJRLegends <MJRLegends@users.noreply.github.com>
2020-01-07 01:51:56 +0000

    Added Jetpack model for Space Suit, added flame particles too

--- src/main/java/com/mjr/extraplanets/items/armor/bases/JetpackArmorBase.java
+++ src/main/java/com/mjr/extraplanets/items/armor/bases/JetpackArmorBase.java
@@ -1,6 +1,7 @@
 package com.mjr.extraplanets.items.armor.bases;
 
 import com.mjr.extraplanets.api.item.IJetpackArmour;
+import com.mjr.extraplanets.client.model.SpaceSuitJetPackFlame;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -18,9 +19,12 @@ public abstract class JetpackArmorBase extends ElectricArmorBase implements IJet
 	public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
 		if (stack.hasTagCompound() && stack.getTagCompound().getBoolean("active") && this.getElectricityStored(stack) >= this.powerPerTick()) {
 			useJetPack(player);
-			if (player instanceof EntityPlayerMP && !((EntityPlayerMP) player).isCreative())
+			if (player instanceof EntityPlayerMP && !((EntityPlayerMP) player).isCreative()) {
 				if ((player.ticksExisted - 1) % 20 == 0)
 					this.discharge(stack, this.powerPerTick(), true);
+			} else if (world.isRemote) {
+				SpaceSuitJetPackFlame.spawnFlames(world, player, stack);
+			}
 		}
 	}
 
