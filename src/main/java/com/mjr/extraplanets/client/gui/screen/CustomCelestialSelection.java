94ad697dda2002c735a87cef25e98522d1b4fe44
MJRLegends <MJRLegends@users.noreply.github.com>
2020-03-31 00:22:19 +0100

    Fixes for MC 1.11.2 & below

--- src/main/java/com/mjr/extraplanets/client/gui/screen/CustomCelestialSelection.java
+++ src/main/java/com/mjr/extraplanets/client/gui/screen/CustomCelestialSelection.java
@@ -1005,7 +1005,7 @@ public class CustomCelestialSelection extends GuiCelestialSelection {
 		}
 		if (Loader.isModLoaded("planetprogression")) {
 			CustomCelestialSelection.drawRect(LHS + 1, (height - LHS) - 5, LHS + 500, (height - LHS)- 20, ColorUtil.to32BitColor(255, 0, 0, 0));
-			this.fontRenderer.drawString("Important: Want to unlock/see more celestial bodies? Research them via PlanetProgressions Mod", LHS + 5, (height - LHS) - 15, RED);
+			this.fontRendererObj.drawString("Important: Want to unlock/see more celestial bodies? Research them via PlanetProgressions Mod", LHS + 5, (height - LHS) - 15, RED);
 		}
 	}
 
