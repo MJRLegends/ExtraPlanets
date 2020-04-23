05d94678a47937ecf307eef39e0aae27cc9399ac
MJRLegends <MJRLegends@users.noreply.github.com>
2020-04-23 01:56:34 +0100

    Updated Item Models to use GC new method of getClientTimeTotal and not getMilliseconds

--- src/main/java/com/mjr/extraplanets/client/model/rockets/ItemModelRocketT7.java
+++ src/main/java/com/mjr/extraplanets/client/model/rockets/ItemModelRocketT7.java
@@ -55,7 +55,7 @@ public class ItemModelRocketT7 extends ModelTransformWrapper {
 			mul.setTranslation(trans);
 			ret.mul(mul);
 			mul.setIdentity();
-			mul.rotY(ClientUtil.getMilliseconds() / 1000.0F);
+			mul.rotY(ClientUtil.getClientTimeTotal() / 1000.0F);
 			ret.mul(mul);
 			mul.setIdentity();
 			trans.scale(-1.0F);
