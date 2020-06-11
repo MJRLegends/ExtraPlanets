46a9e9fbb4fdd88e8f01da77ec1d67ee8f2c6b25
MJRLegends <MJRLegends@users.noreply.github.com>
2020-06-12 00:05:07 +0100

    Fixed issue #545

--- src/main/java/com/mjr/extraplanets/ExtraPlanets.java
+++ src/main/java/com/mjr/extraplanets/ExtraPlanets.java
@@ -290,6 +290,8 @@ public class ExtraPlanets {
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
+		RegisterUtilities.registerEventHandler(new Config());
+		
 		// Register Modules
 		ExtraPlanets_Modules.init();
 
