a9b9581397c47696e0a56c8c8782b710ed2d31a3
MJRLegends <MJRLegends@users.noreply.github.com>
2020-08-10 21:07:07 +0100

    Fixed parent planets not scaling correctly on moon dimensions (Suggestion from #448)

--- src/main/java/com/mjr/extraplanets/moons/Oberon/SkyProviderOberon.java
+++ src/main/java/com/mjr/extraplanets/moons/Oberon/SkyProviderOberon.java
@@ -236,7 +236,7 @@ public class SkyProviderOberon extends IRenderHandler {
 		tessellator1.draw();
 
 		// Render earth
-		f10 = 0.5F;
+		f10 = (sunSize * 1.5F) * 0.25F;
 		GL11.glScalef(0.6F, 0.6F, 0.6F);
 		GL11.glRotatef(40.0F, 0.0F, 0.0F, 1.0F);
 		GL11.glRotatef(200F, 1.0F, 0.0F, 0.0F);
