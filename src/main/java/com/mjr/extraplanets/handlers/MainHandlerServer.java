d05eee2d46cec7b95431a0ecda8cb2db91eb6ffc
MJRLegends <MJRLegends@users.noreply.github.com>
2020-08-11 13:11:11 +0100

    Fixed double amount radiation being slowly reduced on planets

--- src/main/java/com/mjr/extraplanets/handlers/MainHandlerServer.java
+++ src/main/java/com/mjr/extraplanets/handlers/MainHandlerServer.java
@@ -492,8 +492,6 @@ public class MainHandlerServer {
 			if (playerMP != null) {
 				stats = playerMP.getCapability(CapabilityStatsHandler.EP_STATS_CAPABILITY, null);
 			}
-			if ((playerMP.ticksExisted - 1) % 100 == 0 && Config.DEBUG_MODE)
-				MessageUtilities.debugMessageToLog(Constants.modID, "Player Current Radiation Amount: " + stats.getRadiationLevel());
 			if (stats.getRadiationLevel() >= 100) {
 				if ((playerMP.ticksExisted - 1) % 50 == 0)
 					playerMP.attackEntityFrom(DamageSourceEP.radiation, 3F);
@@ -514,6 +512,8 @@ public class MainHandlerServer {
 				stats.setRadiationLevel(stats.getRadiationLevel() + tempLevel);
 			} else
 				stats.setRadiationLevel(0);
+			if ((playerMP.ticksExisted - 1) % 100 == 0 && Config.DEBUG_MODE)
+				MessageUtilities.debugMessageToLog(Constants.modID, "Player Current Radiation Amount: " + stats.getRadiationLevel());
 		}
 	}
 
@@ -525,20 +525,20 @@ public class MainHandlerServer {
 		if ((isInGCDimension || player.world.provider instanceof WorldProviderSpaceStation) && Config.RADIATION) {
 			if (tick % 30 == 0) {
 				this.sendSolarRadiationPacket(player, stats);
-			}
-			if (tick % 30 == 0) {
-				if(Config.RADIATION_SLEEPING_REDUCE_AMOUNT != 0) {
+				if(Config.RADIATION_OVERTIME_REDUCE_AMOUNT != 0) {
 					double temp = stats.getRadiationLevel();
 					double level = (temp * (Config.RADIATION_OVERTIME_REDUCE_AMOUNT * 1.5F)) / 100;
 					if (level <= 0)
 						stats.setRadiationLevel(0);
 					else {
+						if(tick % 100 == 0 && Config.DEBUG_MODE)
+							MessageUtilities.debugMessageToLog(Constants.modID, "Auto Reduced Radiation Amount: " + level);
 						stats.setRadiationLevel(stats.getRadiationLevel() - level);
 					}
 				}
 			}
 		}
-		if (tick % 30 == 0) {
+		else if (tick % 30 == 0) {
 			if(Config.RADIATION_SLEEPING_REDUCE_AMOUNT != 0) {
 				double temp = stats.getRadiationLevel();
 				double level = (temp * Config.RADIATION_OVERTIME_REDUCE_AMOUNT) / 100;
