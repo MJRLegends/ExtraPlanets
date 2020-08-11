01c946909027ff441eaf3671ae45659bc9a32491
MJRLegends <MJRLegends@users.noreply.github.com>
2020-08-11 13:12:15 +0100

    Fixed sounds for Kepler22b Saplings, Kepler22b Tall Grass, Red/Orange Sand, and all Grass blocks issue #586

--- src/main/java/com/mjr/extraplanets/blocks/BlockSand.java
+++ src/main/java/com/mjr/extraplanets/blocks/BlockSand.java
@@ -3,6 +3,7 @@ package com.mjr.extraplanets.blocks;
 import com.mjr.extraplanets.ExtraPlanets;
 
 import net.minecraft.block.BlockFalling;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
 public class BlockSand extends BlockFalling {
@@ -10,6 +11,7 @@ public class BlockSand extends BlockFalling {
 		super(Material.SAND);
 		this.setUnlocalizedName(name);
 		this.setCreativeTab(ExtraPlanets.BlocksTab);
+		this.setSoundType(SoundType.SAND);
 	}
 
 }
