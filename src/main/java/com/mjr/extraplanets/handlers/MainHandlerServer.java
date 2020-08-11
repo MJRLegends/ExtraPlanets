e68d64e222c569c64276f8a5321de8f4d157d1e2
MJRLegends <MJRLegends@users.noreply.github.com>
2020-08-11 03:19:28 +0100

    Fixed gained radiation going negative in certain instances

--- src/main/java/com/mjr/extraplanets/handlers/MainHandlerServer.java
+++ src/main/java/com/mjr/extraplanets/handlers/MainHandlerServer.java
@@ -439,12 +439,12 @@ public class MainHandlerServer {
 		boolean doArmorCheck = false;
 		double damageToTake = 0;
 		double damageModifer = 0;
+		int tierValue = 0;
 
 		ItemStack helmet = playerMP.inventory.armorInventory[3];
 		ItemStack chest = playerMP.inventory.armorInventory[2];
 		ItemStack leggins = playerMP.inventory.armorInventory[1];
 		ItemStack boots = playerMP.inventory.armorInventory[0];
-
 		if (!isValidSpaceSuit(playerMP, helmet, chest, leggins, boots, list, false)) {
 			damageModifer = 0.1;
 			doDamage = true;
@@ -465,7 +465,7 @@ public class MainHandlerServer {
 				MessageUtilities.debugMessageToLog(Constants.modID, "Boots Tier: " + bootsTier);
 			}
 
-			int tierValue = (helmetTier + chestTier + legginsTier + bootsTier) / 2;
+			tierValue = (helmetTier + chestTier + legginsTier + bootsTier) / 2;
 			damageToTake = 0.005 * tierValue;
 			doDamage = true;
 		}
@@ -487,6 +487,8 @@ public class MainHandlerServer {
 				}
 				else {
 					damageModifer = 0.001875 - (damageToTake / 2) / 10;
+					if(damageModifer < 0)
+						damageModifer = 0.000225;
 					tempLevel = damageModifer * (amount / 10) / 6;
 				}
 				if ((playerMP.ticksExisted - 1) % 100 == 0 && Config.DEBUG_MODE)
