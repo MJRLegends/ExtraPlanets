d46709285e9e09b79f5859adfd31af20c92fab3a
MJRLegends <MJRLegends@users.noreply.github.com>
2020-09-11 00:09:28 +0100

    Started work on improving Jupiter's Dungeon

---
+++ src/main/java/com/mjr/extraplanets/planets/Jupiter/worldgen/dungeon/RoomSpawnerJupiter.java
@@ -0,0 +1,86 @@
+package com.mjr.extraplanets.planets.Jupiter.worldgen.dungeon;
+
+import java.util.Random;
+
+import com.mjr.extraplanets.Constants;
+import com.mjr.extraplanets.blocks.fluid.ExtraPlanets_Fluids;
+
+import micdoodle8.mods.galacticraft.core.world.gen.dungeon.DungeonConfiguration;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntityMobSpawner;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+
+public class RoomSpawnerJupiter extends RoomEmptyJupiter
+{
+    public RoomSpawnerJupiter()
+    {
+    }
+
+    public RoomSpawnerJupiter(DungeonConfiguration configuration, Random rand, int blockPosX, int blockPosZ, int sizeX, int sizeY, int sizeZ, EnumFacing entranceDir)
+    {
+        super(configuration, rand, blockPosX, blockPosZ, sizeX, sizeY, sizeZ, entranceDir, 20);
+    }
+
+    @Override
+    public boolean addComponentParts(World worldIn, Random random, StructureBoundingBox boundingBox)
+    {
+        if (super.addComponentParts(worldIn, random, boundingBox))
+        {
+            for (int i = 1; i <= this.sizeX - 1; ++i)
+            {
+                for (int j = 1; j <= this.sizeY - 1; ++j)
+                {
+                    for (int k = 1; k <= this.sizeZ - 1; ++k)
+                    {
+                        if (random.nextFloat() < 0.080F)
+                        {
+                            this.setBlockState(worldIn, Blocks.WEB.getDefaultState(), i, j, k, boundingBox);
+                        }
+                    }
+                }
+            }
+
+            this.setBlockState(worldIn, Blocks.MOB_SPAWNER.getDefaultState(), 2, 0, 2, boundingBox);
+            this.setBlockState(worldIn, Blocks.MOB_SPAWNER.getDefaultState(), this.sizeX - 2, 0, this.sizeZ - 2, boundingBox);
+            
+            for(int i = 1; i < 7; i++)
+            	this.setBlockState(worldIn, ExtraPlanets_Fluids.MAGMA.getDefaultState(), 1, i, 1, boundingBox);
+            for(int i = 1; i < 7; i++)
+            	this.setBlockState(worldIn, ExtraPlanets_Fluids.MAGMA.getDefaultState(), this.sizeX - 1, i, this.sizeZ - 1, boundingBox);
+            for(int i = 1; i < 7; i++)
+            	this.setBlockState(worldIn, ExtraPlanets_Fluids.MAGMA.getDefaultState(), 2, i, 2, boundingBox);
+            for(int i = 1; i < 7; i++)
+            	this.setBlockState(worldIn, ExtraPlanets_Fluids.MAGMA.getDefaultState(), this.sizeX - 2, i, this.sizeZ - 2, boundingBox);
+            
+            BlockPos blockpos = new BlockPos(this.getXWithOffset(2, 2), this.getYWithOffset(0), this.getZWithOffset(2, 2));
+            TileEntityMobSpawner spawner = (TileEntityMobSpawner) worldIn.getTileEntity(blockpos);
+
+            if (spawner != null)
+            {
+                spawner.getSpawnerBaseLogic().setEntityId(getMob(random));
+            }
+
+            blockpos = new BlockPos(this.getXWithOffset(this.sizeX - 2, this.sizeZ - 2), this.getYWithOffset(0), this.getZWithOffset(this.sizeX - 2, this.sizeZ - 2));
+            spawner = (TileEntityMobSpawner) worldIn.getTileEntity(blockpos);
+
+            if (spawner != null)
+            {
+                spawner.getSpawnerBaseLogic().setEntityId(getMob(random));
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private static ResourceLocation getMob(Random rand)
+    {
+        return new ResourceLocation(Constants.modID, Constants.modID + "." + "EvolvedAncientMagmaCube");
+    }
+}
---
+++ src/main/java/com/mjr/extraplanets/planets/Jupiter/worldgen/dungeon/RoomTreasureJupiterBase.java
@@ -0,0 +1,146 @@
+package com.mjr.extraplanets.planets.Jupiter.worldgen.dungeon;
+
+import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
+import micdoodle8.mods.galacticraft.core.Constants;
+import micdoodle8.mods.galacticraft.core.GCBlocks;
+import micdoodle8.mods.galacticraft.core.blocks.BlockTier1TreasureChest;
+import micdoodle8.mods.galacticraft.core.tile.TileEntityTreasureChest;
+import micdoodle8.mods.galacticraft.core.world.gen.dungeon.DungeonConfiguration;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+import net.minecraft.world.gen.structure.StructureComponent;
+import net.minecraft.world.storage.loot.LootTableList;
+
+import java.util.Random;
+
+import com.mjr.extraplanets.planets.Jupiter.worldgen.RoomBossJupiter;
+
+public class RoomTreasureJupiterBase extends SizedPieceJupiter
+{
+    public static ResourceLocation MOONCHEST = new ResourceLocation(Constants.ASSET_PREFIX, "dungeon_Jupiter");
+    public static final ResourceLocation TABLE_TIER_1_DUNGEON = LootTableList.register(MOONCHEST);
+
+    public RoomTreasureJupiterBase()
+    {
+    }
+
+    public RoomTreasureJupiterBase(DungeonConfiguration configuration, Random rand, int blockPosX, int blockPosZ, EnumFacing entranceDir)
+    {
+        this(configuration, rand, blockPosX, blockPosZ, rand.nextInt(4) + 6, configuration.getRoomHeight(), rand.nextInt(4) + 6, entranceDir);
+    }
+
+    public RoomTreasureJupiterBase(DungeonConfiguration configuration, Random rand, int blockPosX, int blockPosZ, int sizeX, int sizeY, int sizeZ, EnumFacing entranceDir)
+    {
+        super(configuration, sizeX, sizeY, sizeZ, entranceDir.getOpposite());
+        this.setCoordBaseMode(EnumFacing.SOUTH);
+        int yPos = configuration.getYPosition();
+
+        this.boundingBox = new StructureBoundingBox(blockPosX, yPos, blockPosZ, blockPosX + this.sizeX, yPos + this.sizeY, blockPosZ + this.sizeZ);
+    }
+
+    @Override
+    public boolean addComponentParts(World worldIn, Random random, StructureBoundingBox chunkBox)
+    {
+        for (int i = 0; i <= this.sizeX; i++)
+        {
+            for (int j = 0; j <= this.sizeY; j++)
+            {
+                for (int k = 0; k <= this.sizeZ; k++)
+                {
+                    if (i == 0 || i == this.sizeX || j == 0 || j == this.sizeY || k == 0 || k == this.sizeZ)
+                    {
+                        boolean placeBlock = true;
+                        if (getDirection().getAxis() == EnumFacing.Axis.Z)
+                        {
+                            int start = (this.boundingBox.maxX - this.boundingBox.minX) / 2 - 1;
+                            int end = (this.boundingBox.maxX - this.boundingBox.minX) / 2 + 1;
+                            if (i > start && i <= end && j < 3 && j > 0)
+                            {
+                                if (getDirection() == EnumFacing.SOUTH && k == 0)
+                                {
+                                    placeBlock = false;
+                                }
+                                else if (getDirection() == EnumFacing.NORTH && k == this.sizeZ)
+                                {
+                                    placeBlock = false;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            int start = (this.boundingBox.maxZ - this.boundingBox.minZ) / 2 - 1;
+                            int end = (this.boundingBox.maxZ - this.boundingBox.minZ) / 2 + 1;
+                            if (k > start && k <= end && j < 3 && j > 0)
+                            {
+                                if (getDirection() == EnumFacing.EAST && i == 0)
+                                {
+                                    placeBlock = false;
+                                }
+                                else if (getDirection() == EnumFacing.WEST && i == this.sizeX)
+                                {
+                                    placeBlock = false;
+                                }
+                            }
+                        }
+                        if (placeBlock)
+                        {
+                            this.setBlockState(worldIn, this.configuration.getBrickBlock(), i, j, k, chunkBox);
+                        }
+                        else
+                        {
+                            this.setBlockState(worldIn, Blocks.AIR.getDefaultState(), i, j, k, chunkBox);
+                        }
+                    }
+                    else if ((i == 1 && k == 1) || (i == 1 && k == this.sizeZ - 1) || (i == this.sizeX - 1 && k == 1) || (i == this.sizeX - 1 && k == this.sizeZ - 1))
+                    {
+                        this.setBlockState(worldIn, Blocks.GLOWSTONE.getDefaultState(), i, j, k, chunkBox);
+                    }
+                    else if (i == this.sizeX / 2 && j == 1 && k == this.sizeZ / 2)
+                    {
+                        BlockPos blockpos = new BlockPos(this.getXWithOffset(i, k), this.getYWithOffset(j), this.getZWithOffset(i, k));
+                        if (chunkBox.isVecInside(blockpos))
+                        {
+                            worldIn.setBlockState(blockpos, GCBlocks.treasureChestTier1.getDefaultState().withProperty(BlockTier1TreasureChest.FACING, this.getDirection().getOpposite()), 2);
+                            TileEntityTreasureChest treasureChest = (TileEntityTreasureChest) worldIn.getTileEntity(blockpos);
+                            if (treasureChest != null)
+                            {
+                                ResourceLocation chesttype = TABLE_TIER_1_DUNGEON;
+                                if (worldIn.provider instanceof IGalacticraftWorldProvider)
+                                {
+                                    chesttype = ((IGalacticraftWorldProvider)worldIn.provider).getDungeonChestType();
+                                }
+                                treasureChest.setLootTable(chesttype, random.nextLong());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        this.setBlockState(worldIn, Blocks.AIR.getDefaultState(), i, j, k, chunkBox);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public PieceJupiter getNextPiece(DungeonStartJupiter startPiece, Random rand)
+    {
+        if (startPiece.attachedComponents.size() > 2)
+        {
+            StructureComponent component = startPiece.attachedComponents.get(startPiece.attachedComponents.size() - 3);
+            if (component instanceof RoomBossJupiter)
+            {
+                BlockPos blockpos = new BlockPos(this.getXWithOffset(this.sizeX / 2, this.sizeZ / 2), this.getYWithOffset(1), this.getZWithOffset(this.sizeX / 2, this.sizeZ / 2));
+                ((RoomBossJupiter) component).setChestPos(new BlockPos(blockpos));
+            }
+        }
+        return null;
+    }
+}
