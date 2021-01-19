b8a225394ad60b0534034747a7cc268183ebc8dc
MJRLegends <MJRLegends@users.noreply.github.com>
2021-01-19 02:41:25 +0000

    Added IC2 quantum armour to be default entered for "List of armour items to be considered as a space suit" in config (Will only work for new generated configs) & tweaked nano armour values

--- src/main/java/com/mjr/extraplanets/Config.java
+++ src/main/java/com/mjr/extraplanets/Config.java
@@ -764,10 +764,14 @@ public class Config {
 		RADIATION_ANTI_RAD_REDUCE_AMOUNT = config.get(Constants.CONFIG_CATEGORY_PRESSURE_RADIATION_SETTINGS, "Amount of Radiation reduced by Anti Rad Drinks", 50, "[range: -2147483647 ~ 2147483647, default: 50]").getInt();
 
 		SPACE_SUIT_SUPPORTED_ARMOUR = config.get(Constants.CONFIG_CATEGORY_PRESSURE_RADIATION_SETTINGS, "List of armour items to be considered as a space suit", new String[] {
-				"ic2:nano_boots:3",
-				"ic2:nano_chestplate:3",
-				"ic2:nano_helmet:3",
-				"ic2:nano_leggings:3",
+				"ic2:nano_boots:2",
+				"ic2:nano_chestplate:2",
+				"ic2:nano_helmet:2",
+				"ic2:nano_leggings:2",
+				"ic2:quantum_boots:3",
+				"ic2:quantum_chestplate:3",
+				"ic2:quantum_helmet:3",
+				"ic2:quantum_leggings:3",
 				"galaxyspace:space_suit_head:1",
 				"galaxyspace:space_suit_chest:1",
 				"galaxyspace:space_suit_legs:1",
