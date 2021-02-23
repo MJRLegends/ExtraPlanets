a50d9bf3de5b1adda7388155b1544edfd7a2a11b
MJRLegends <MJRLegends@users.noreply.github.com>
2021-02-23 01:12:09 +0000

    Fixed a crash with the BasicDensifier when restarting world during a machine operating

--- src/main/java/com/mjr/extraplanets/tileEntities/machines/TileEntityBasicDensifier.java
+++ src/main/java/com/mjr/extraplanets/tileEntities/machines/TileEntityBasicDensifier.java
@@ -160,6 +160,8 @@ public class TileEntityBasicDensifier extends TileBaseElectricBlockWithInventory
 	}
 
 	public void updateOutput() {
+		if (this.inputTank.getFluid() == null)
+			return;
 		if (this.inputTank.getFluid().equals(new FluidStack(ExtraPlanets_Fluids.LIQUID_CARAMEL_FLUID, 0)))
 			this.producingStack = new ItemStack(ExtraPlanets_Items.CARAMEL_BAR, 6);
 		else if (this.inputTank.getFluid().equals(new FluidStack(ExtraPlanets_Fluids.LIQUID_CHOCOLATE_FLUID, 0)))
@@ -269,6 +271,10 @@ public class TileEntityBasicDensifier extends TileBaseElectricBlockWithInventory
 		super.readFromNBT(nbt);
 		this.processTicks = nbt.getInteger("smeltingTicks");
 		this.stacks = this.readStandardItemsFromNBT(nbt);
+		if(this.producingStack == null && nbt.hasKey("producingStack")) {
+	        final ItemStack itemStack = new ItemStack((NBTTagCompound) nbt.getTag("producingStack"));
+	        this.producingStack = itemStack;
+		}
 
 		if (nbt.hasKey("inputTank")) {
 			this.inputTank.readFromNBT(nbt.getCompoundTag("inputTank"));
@@ -280,10 +286,12 @@ public class TileEntityBasicDensifier extends TileBaseElectricBlockWithInventory
 		super.writeToNBT(nbt);
 		nbt.setInteger("smeltingTicks", this.processTicks);
 		this.writeStandardItemsToNBT(nbt, this.stacks);
+		nbt.setTag("producingStack", this.producingStack.serializeNBT());
 
 		if (this.inputTank.getFluid() != null) {
 			nbt.setTag("inputTank", this.inputTank.writeToNBT(new NBTTagCompound()));
 		}
+		producingStack.serializeNBT();
 		return nbt;
 	}
 
