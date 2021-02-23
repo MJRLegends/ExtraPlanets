e24bd36fc5b6086b9180d0e672eb6a4037886144
MJRLegends <MJRLegends@users.noreply.github.com>
2021-02-23 00:49:43 +0000

    Added support to use ExtraPlanets Radiation System on other addon planets/moons

--- src/main/java/com/mjr/extraplanets/ExtraPlanets.java
+++ src/main/java/com/mjr/extraplanets/ExtraPlanets.java
@@ -95,6 +95,9 @@ import com.mjr.mjrlegendslib.util.NetworkUtilities;
 import com.mjr.mjrlegendslib.util.RegisterUtilities;
 
 import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
+import micdoodle8.mods.galacticraft.api.galaxies.GalaxyRegistry;
+import micdoodle8.mods.galacticraft.api.galaxies.Moon;
+import micdoodle8.mods.galacticraft.api.galaxies.Planet;
 import micdoodle8.mods.galacticraft.api.recipe.SchematicRegistry;
 import micdoodle8.mods.galacticraft.core.GCItems;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
@@ -395,6 +398,31 @@ public class ExtraPlanets {
 		ExtraPlanetsDimensions.KEPLER22B_ORBIT_KEEPLOADED = DimensionType.register("Kepler22b Space Station", "orbit", Config.KEPLER22B_SPACE_STATION_STATIC_ID, WorldProviderKepler22bOrbit.class, true);
 		ExtraPlanetsDimensions.MARS_ORBIT_KEEPLOADED = DimensionType.register("Mars Space Station", "_mars_orbit", Config.MARS_SPACE_STATION_STATIC_ID, WorldProviderMarsOrbit.class, true);
 		ExtraPlanetsDimensions.VENUS_ORBIT_KEEPLOADED = DimensionType.register("Venus Space Station", "_venus_orbit", Config.VENUS_SPACE_STATION_STATIC_ID, WorldProviderVenusOrbit.class, true);
+
+		if (Config.OTHER_ADDON_PLANET_MOON_RAD_VALUES != null) {
+			List<String> entries = new ArrayList<>();
+			for (Planet planet : GalaxyRegistry.getRegisteredPlanets().values()) {
+				if(planet.getUnlocalizedName().contains("overworld") || planet.atmosphere.isBreathable())
+					continue;
+				if (planet.getWorldProvider() != null && !planet.getWorldProvider().getName().contains("com.mjr.extraplanets") && !planet.getWorldProvider().getName().contains("micdoodle8.mods.galacticraft")) {
+					entries.add(planet.getUnlocalizedName() + ":10");
+				}
+			}
+
+			for (Moon planet : GalaxyRegistry.getRegisteredMoons().values()) {
+				if(planet.atmosphere.isBreathable())
+					continue;
+				if (planet.getWorldProvider() != null && !planet.getWorldProvider().getName().contains("com.mjr.extraplanets") && !planet.getWorldProvider().getName().contains("micdoodle8.mods.galacticraft")) {
+					entries.add(planet.getUnlocalizedName() + ":10");
+				}
+			}
+
+			Config.OTHER_ADDON_PLANET_MOON_RAD_VALUES = entries.stream().toArray(String[]::new);
+			Config.config.get(Constants.CONFIG_CATEGORY_PRESSURE_RADIATION_SETTINGS, "Other Addon Planets/Moons Radiation Values", new String[] {},
+					"Format: 'bodyName:radiationValue' (radiationValue = 0 = Disabled. range: 0 ~ 100, default: 10) | example: planet.atheon:12").set(Config.OTHER_ADDON_PLANET_MOON_RAD_VALUES);
+			Config.config.save();
+			Config.updateOtherModRadiationValues();
+		}
 
 		// Proxy PostInit Method
 		ExtraPlanets.proxy.postInit(event);
