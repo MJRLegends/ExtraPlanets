b3c0c04c34d527439a0da496aa590f4a3b53fc22
MJRLegends <MJRLegends@users.noreply.github.com>
2021-07-19 01:01:59 +0100

    Fixed a few rare possible world gen issues with Ganymede & Europa

--- src/main/java/com/mjr/extraplanets/moons/Europa/worldgen/BiomeDecoratorEuropa.java
+++ src/main/java/com/mjr/extraplanets/moons/Europa/worldgen/BiomeDecoratorEuropa.java
@@ -78,12 +78,12 @@ public class BiomeDecoratorEuropa extends BiomeDecoratorSpace {
 		MinecraftForge.EVENT_BUS.post(new DecorateBiomeEvent.Pre(this.currentWorld, this.rand, new BlockPos(this.posX, 0, this.posZ)));
 		for (int i = 0; i < this.LakesPerChunk; i++) {
 			if (this.rand.nextInt(10) == 0) {
-				WorldGenUtilities.generateLake(this.currentWorld, this.rand, new BlockPos(this.posX, 0, this.posZ), ExtraPlanets_Fluids.SALT, ExtraPlanets_Blocks.CERES_BLOCKS);
+				WorldGenUtilities.generateLake(this.currentWorld, this.rand, new BlockPos(this.posX, 0, this.posZ), ExtraPlanets_Fluids.SALT, ExtraPlanets_Blocks.EUROPA_BLOCKS);
 			}
 		}
 		for (int i = 0; i < 1; i++) {
 			if (this.rand.nextInt(100) == 0) {
-				WorldGenUtilities.generateLake(this.currentWorld, this.rand, new BlockPos(this.posX, 0, this.posZ), ExtraPlanets_Fluids.RADIO_ACTIVE_WATER, ExtraPlanets_Blocks.CERES_BLOCKS);
+				WorldGenUtilities.generateLake(this.currentWorld, this.rand, new BlockPos(this.posX, 0, this.posZ), ExtraPlanets_Fluids.RADIO_ACTIVE_WATER, ExtraPlanets_Blocks.EUROPA_BLOCKS);
 			}
 		}
 		if (Config.GENERATE_EUROPA_IRON_CHUNKS) {
--- src/main/java/com/mjr/extraplanets/moons/Ganymede/worldgen/BiomeDecoratorGanymede.java
+++ src/main/java/com/mjr/extraplanets/moons/Ganymede/worldgen/BiomeDecoratorGanymede.java
@@ -72,7 +72,7 @@ public class BiomeDecoratorGanymede extends BiomeDecoratorSpace {
 		MinecraftForge.EVENT_BUS.post(new DecorateBiomeEvent.Pre(this.currentWorld, this.rand, new BlockPos(this.posX, 0, this.posZ)));
 		for (int i = 0; i < 1; i++) {
 			if (this.rand.nextInt(100) == 0) {
-				WorldGenUtilities.generateLake(this.currentWorld, this.rand, new BlockPos(this.posX, 0, this.posZ), ExtraPlanets_Fluids.INFECTED_WATER, ExtraPlanets_Blocks.MERCURY_BLOCKS);
+				WorldGenUtilities.generateLake(this.currentWorld, this.rand, new BlockPos(this.posX, 0, this.posZ), ExtraPlanets_Fluids.INFECTED_WATER, ExtraPlanets_Blocks.GANYMEDE_BLOCKS);
 			}
 		}
 		if (Config.GENERATE_GANYMEDE_ICE_CHUNKS) {
